97b21b88dfeee9920f164721301ac785f5789e51
Dheeraj Shetty <dshetty@codeaurora.org>
2018-08-07 13:19:48 -0700

    Add make file for stubs
    Add a cmake make file to compile each of the stub library.
    Add a print stating that this is not the real library.
    
    Change-Id: Ic5bcb103ee512118bc732d880af36320b24bc637

--- bin/stubs.c
+++ bin/stubs.c
@@ -1,6 +1,7 @@
 //Dummy file for stub library compilation
+#include <stdio.h>
 
-extern "C" {
 void FNAME () {
-}
+    fprintf(stderr, "%s: %s\n", __func__, "This is not the real telux library.");
+    abort();
 }

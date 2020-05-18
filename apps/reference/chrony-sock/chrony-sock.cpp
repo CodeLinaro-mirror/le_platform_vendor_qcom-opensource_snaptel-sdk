311386ac8f2781e3a20cee214ee097d0cb355a5d
Gurtaj Singh <gurtsing@codeaurora.org>
2020-05-18 17:41:07 +0530

    telux: Change chrony fix request to 10Hz
    GNSS receiver transition penalty when switching between fix intervals.
    All client was required to switch to 10Hz fix request for KPI
    improvement.
    
    Change-Id: I61c36899db26320b050ead2b47fdae3aaab579a6

--- apps/reference/chrony-sock/chrony-sock.cpp
+++ apps/reference/chrony-sock/chrony-sock.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2019, The Linux Foundation. All rights reserved.
+ *  Copyright (c) 2019-2020, The Linux Foundation. All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions are
@@ -315,7 +315,7 @@ int main(int argc, char *argv[]) {
         return -EINVAL;
     }
 
-    status = locationManager->startBasicReports(0, 1000, responseCallback);
+    status = locationManager->startBasicReports(0, 100, responseCallback);
     if (status != Status::SUCCESS) {
         LOGE("Failed to start basic location reports\n");
         return -EINVAL;

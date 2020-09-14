08387f48bd33e244091b86df1bf5d7f63e58e27c
Siddharth Dewangan <sdewanga@codeaurora.org>
2020-09-14 10:58:04 +0530

    telux: Added support for year of hardware in location sdk
    Updated the interface to support API for year of hardware.
    Updated application to support year of hardware information.
    
    Change-Id: I210662ae95030801b7c641acc50bdf51459357df

--- apps/tests/location_test_app/LocationMenu.hpp
+++ apps/tests/location_test_app/LocationMenu.hpp
@@ -82,6 +82,7 @@ public:
    void configureRobustLocation(std::vector<std::string> userInput);
    void requestRobustLocation(std::vector<std::string> userInput);
    void requestEnergyConsumedInfo(std::vector<std::string> userInput);
+   void getYearOfHw(std::vector<std::string> userInput);
    void configureMinGpsWeek(std::vector<std::string> userInput);
    void configureMinSVElevation(std::vector<std::string> userInput);
    void requestMinGpsWeek(std::vector<std::string> userInput);

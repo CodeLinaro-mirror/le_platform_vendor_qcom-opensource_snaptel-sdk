92276291e23bd3efe660699615014adec2429fed
Ambika Pavan <apavan@qti.qualcomm.com>
2021-01-11 20:19:14 +0530

    telux: Add API to support hangup of multi-party calls.
    1. Add support in telsdk console app to hangup multi-party calls
       (conference in CS) at once instead of hang up by index each call.
    2. This API also supports hanging up the foreground call(s) and
       resuming the background call.
    3. Add info whether the call is in conference or not in progress calls.
    
    CRs-Fixed: 2842393
    
    Change-Id: Id3ce0cb829fd52f84ad2ff5748ae878c7c97c3d4

--- apps/tests/telsdk_console_app/Call/MyCallListener.hpp
+++ apps/tests/telsdk_console_app/Call/MyCallListener.hpp
@@ -27,6 +27,12 @@
  *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/*
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
 #ifndef MYCALLLISTENER_HPP
 #define MYCALLLISTENER_HPP
 
@@ -67,6 +73,11 @@ public:
 
 private:
    std::string commandName_;
+};
+
+class MyHangupCallback {
+public:
+   static void hangupFgResumeBgResponse(telux::common::ErrorCode error);
 };
 
 #endif  // MYCALLLISTENER_HPP

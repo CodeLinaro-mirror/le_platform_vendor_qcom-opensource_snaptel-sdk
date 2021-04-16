ef549ead01b6ef6de26a8f14b592d7f41f635023
Siva Kumar Mellemputi <smellemp@codeaurora.org>
2021-04-16 20:37:15 +0530

    telux: Added subsystem initialization APIs for SMS Manager
    Issue:
    - As per the existing logic, apps are expected to use SMS Manger after
      making sure that PhoneManager is initialized. However this dependency
      exists to validate the phoneId input, which can be removed.
    - If an application sends an SMS immediately after PhoneManager initialization,
      it could fail as IMS registration state info may not be initialized yet.
    
    Fix:
    - Remove SMS Manager dependency on PhoneManager to validate the phoneId
    - Add new subsystem initialization APIs which the apps can rely on, before
      sending the SMS
    - SMS Manager establishes RIL connection and gets IMS registration state info
      as part of initialization
    
    CRs-Fixed: 2920348
    
    Change-Id: I888ee860870ec2b5987e2c79e44261ac3767147e

--- include/telux/tel/PhoneFactory.hpp
+++ include/telux/tel/PhoneFactory.hpp
@@ -85,10 +85,15 @@ public:
     * SMS messages.
     *
     * @param [in] phoneId   Unique identifier for the phone
+    * @param [in] callback  Optional callback pointer to get response of SMS Manager initialization.
+    *                       It will be invoked when initialization is either succeeded or failed.
+    *                       In case of failure response, the provided SMS Manager object will no
+    *                       more be a valid object.
     *
     * @returns Pointer of ISmsManager object or nullptr in case of failure.
     */
-   std::shared_ptr<ISmsManager> getSmsManager(int phoneId = DEFAULT_PHONE_ID);
+   std::shared_ptr<ISmsManager> getSmsManager(int phoneId = DEFAULT_PHONE_ID,
+      telux::common::InitResponseCb callback = nullptr);
 
    /**
     * Get Call Manager instance to determine state of active calls and perform
@@ -212,8 +217,11 @@ private:
    std::map<int, std::shared_ptr<IRemoteSimManager>> remoteSimManagerMap_;
    std::vector<telux::common::InitResponseCb> imssCallbacks_;
    telux::common::ServiceStatus imssInitStatus_;
+   std::map<int, std::vector<telux::common::InitResponseCb>> smsMgrCallbacks_;
+   std::map<int, telux::common::ServiceStatus> smsMgrInitStatus_;
 
    void onImsSettingsManagerResponse(telux::common::ServiceStatus status);
+   void onSmsMgrInitResponse(int phoneId, telux::common::ServiceStatus status);
 
    PhoneFactory();
    ~PhoneFactory();

8c0d0d82c11884f4a0b55977cd09784f576b339e
Siva Kumar Mellemputi <smellemp@codeaurora.org>
2021-05-01 14:34:45 +0530

    telux: Fix to get TcuActivityManager instance for a clientType
    Issue:
    - If an application requests for two TcuActivityManager instances
      of different ClientType but with same ProcType, it will get the
      same object/instance.
    - This is because the current implementation creates objects per
      ProcType, but not ClientType
    
    Fix:
    - TcuActivityManager objects are now created based on both ProcType
      and ClientType. Hence the application would get different objects
      as expected
    
    CRs-Fixed: 2940854
    
    Change-Id: I1f9aba6105dd20219c1575793ddc4d070bd4a147

--- include/telux/power/PowerFactory.hpp
+++ include/telux/power/PowerFactory.hpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2019-2020, The Linux Foundation. All rights reserved.
+ *  Copyright (c) 2019-2021, The Linux Foundation. All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions are
@@ -74,7 +74,8 @@ public:
         common::ProcType procType = common::ProcType::LOCAL_PROC);
 
 private:
-    std::map<common::ProcType, std::shared_ptr<ITcuActivityManager>> tcuActivityManagerMap_;
+    std::map<std::pair<common::ProcType, ClientType>,
+        std::shared_ptr<ITcuActivityManager>> tcuActivityManagerMap_;
     std::mutex tcuActivityFactoryMutex_;
     PowerFactory();
     PowerFactory(const PowerFactory &) = delete;

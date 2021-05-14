c2c28657a7663f16968718ab02e4e164dbfc5d49
Yujing Gu <quic_yujingg@quicinc.com>
2021-05-14 15:51:38 +0800

    telux: Add common API to get platform version
    1.Add common API to get modem, meta and app version.
    2.Add test app to request and print the platform and sdk versions.
    
    Change-Id: I6d798f6c02c1a85e1dc6339b859b7d6d57cc33a9

--- include/telux/common/Version.hpp
+++ include/telux/common/Version.hpp
@@ -27,6 +27,42 @@
  *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/*
+ * Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the
+ * disclaimer below) provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *    * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 /**
  * @file       Version.hpp
  * @brief      Provide APIs to query the version of the SDK
@@ -36,6 +72,7 @@
 #define VERSION_HPP
 
 #include <string>
+#include "CommonDefines.hpp"
 
 #define DEFAULT_VALUE -1
 
@@ -58,6 +95,20 @@ struct SdkVersion {
 };
 
 /**
+ * Structure contains the version of the platform software
+ */
+struct PlatformVersion {
+   std::string meta; /**< Meta Version,
+                                for example: SA2150P_SA515M.LE_LE.1-3_2-1-00297-STD.INT-1*/
+   std::string modem; /**< Modem Version,
+                                for example: MPSS.HI.3.1.c3-00114-SDX55_GENAUTO_TEST-1*/
+   std::string externalApp; /**< External App Version,
+                                for example: LE.UM.3.2.3-72102-SA2150p.Int-1*/
+   std::string integratedApp; /**< Integrated App MDM Version,
+                                for example: LE.UM.4.1.1-71802-sa515m.Int-1*/
+};
+
+/**
  * @brief Provides version of SDK.
  */
 class Version {
@@ -75,6 +126,19 @@ public:
     * @returns @ref SdkVersion structure of major, minor and patch version
     */
    static SdkVersion getSdkVersion();
+
+   /**
+    * Get the platform version.
+    * Need obtain required permissions from telux_allow_version.
+    *
+    * @param[in] @ref PlatformVersion structure of modem version, meta version, apps version and
+    *                 apps mdm version.
+    * @returns Status of getPlatformVersion i.e. success or suitable error code.
+    *
+    * @note Eval: This is a new API and is being evaluated.It is subject to change
+    *       and could break backwards compatibility.
+    */
+   static Status getPlatformVersion(PlatformVersion & pv);
 };
 /** @} */ /* end_addtogroup telematics_common */
 }

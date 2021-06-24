d988c2c93890593b9132598ab5492739ea56d31d
Gurtaj Singh <gurtsing@codeaurora.org>
2021-06-24 14:34:04 +0530

    snaptel-sdk: Fix checking of promise always instead of only Service available
    - In Data filter menu we are only checking promise if service is not
      available, ideally should be check for all the scenario.
    
    - Use separate promises for DCM and DFS
    
    CRs-Fixed: 2976237
    Change-Id: Ia25d79c41db1f58b8c27c271c8e44321aef181d5

--- apps/tests/telsdk_console_app/Data/data_filter/DataFilterMenu.cpp
+++ apps/tests/telsdk_console_app/Data/data_filter/DataFilterMenu.cpp
@@ -78,13 +78,13 @@ bool DataFilterMenu::initializeSDK() {
     SlotId slotId = DEFAULT_SLOT_ID;
     std::chrono::time_point<std::chrono::system_clock> startTime, endTime;
     startTime = std::chrono::system_clock::now();
-    std::promise<telux::common::ServiceStatus> prom{};
+    std::promise<telux::common::ServiceStatus> dcmProm{};
 
     // Get the DataFactory instances.
     auto &dataFactory = telux::data::DataFactory::getInstance();
 
     dataConnectionManager_ = dataFactory.getDataConnectionManager(slotId,
-        [&prom](telux::common::ServiceStatus status) { prom.set_value(status); });
+        [&dcmProm](telux::common::ServiceStatus status) { dcmProm.set_value(status); });
 
     if (!dataConnectionManager_) {
         std::cout << "Failed to get DataManager object" << std::endl;
@@ -92,12 +92,9 @@ bool DataFilterMenu::initializeSDK() {
     }
 
     if (dataConnectionManager_) {
-        subSystemStatus = dataConnectionManager_->getServiceStatus();
-        if (subSystemStatus != telux::common::ServiceStatus::SERVICE_AVAILABLE) {
-            std::cout << "\n\nInitializing Data connection manager subsystem on slot " <<
-                DEFAULT_SLOT_ID << ", Please wait ..." << endl;
-            subSystemStatus = prom.get_future().get();
-        }
+        std::cout << "\n\nInitializing Data connection manager subsystem on slot " <<
+            DEFAULT_SLOT_ID << ", Please wait ..." << endl;
+        subSystemStatus = dcmProm.get_future().get();
 
         if (subSystemStatus == telux::common::ServiceStatus::SERVICE_AVAILABLE) {
             std::cout << "\nData Connection Manager on slot "<< slotId << " is ready"
@@ -111,10 +108,10 @@ bool DataFilterMenu::initializeSDK() {
         }
     }
     subSystemStatusUpdated_ = false;
-    prom = std::promise<telux::common::ServiceStatus>();
+    std::promise<telux::common::ServiceStatus> dfsProm{};
     // Get data filter manager object
     dataFilterMgr_ = dataFactory.getDataFilterManager(DEFAULT_SLOT_ID,
-        [&prom](telux::common::ServiceStatus status) { prom.set_value(status); });
+        [&dfsProm](telux::common::ServiceStatus status) { dfsProm.set_value(status); });
     if (dataFilterMgr_ == nullptr) {
         std::cout << "WARNING: Data Filter feature is not supported." << std::endl;
         return false;
@@ -122,11 +119,9 @@ bool DataFilterMenu::initializeSDK() {
 
     if (dataFilterMgr_) {
         subSystemStatus = dataFilterMgr_->getServiceStatus();
-        if (subSystemStatus != telux::common::ServiceStatus::SERVICE_AVAILABLE) {
-            std::cout << "\n\nInitializing Data filter manager subsystem on slot " <<
-                DEFAULT_SLOT_ID << ", Please wait ..." << endl;
-            subSystemStatus = prom.get_future().get();
-        }
+        std::cout << "\n\nInitializing Data filter manager subsystem on slot " <<
+            DEFAULT_SLOT_ID << ", Please wait ..." << endl;
+        subSystemStatus = dfsProm.get_future().get();
 
         if (subSystemStatus == telux::common::ServiceStatus::SERVICE_AVAILABLE) {
             std::cout << "\nData Filter Manager on slot "<< slotId << " is ready" << std::endl;

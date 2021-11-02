a3403395b6d5314ba97fedbc4c82752e5cf9beda
Siddharth Dewangan <quic_sdewanga@quicinc.com>
2021-11-02 15:37:09 +0530

    telux: Added support for boot timestamp in location sdk
    Added validity for boot timestamp and relevant APIs.
    Updated app accordingly.
    
    Change-Id: Ic7aaa9717c0d20cf13d0919f8ec9eadf532c97c3

--- include/telux/loc/LocationDefines.hpp
+++ include/telux/loc/LocationDefines.hpp
@@ -27,6 +27,42 @@
  *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/*
+ *  Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ *  Copyright (c) 2021 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted (subject to the limitations in the
+ *  disclaimer below) provided that the following conditions are met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *    * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *
+ *   * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 /**
  * @file       LocationDefines.hpp
  *
@@ -734,7 +770,11 @@ enum LocationValidityType {
     /** Location has valid heading accuracy.*/
     HAS_HEADING_ACCURACY_BIT  = (1<<7),
     /** Location has valid timestamp.*/
-    HAS_TIMESTAMP_BIT         = (1<<8)
+    HAS_TIMESTAMP_BIT         = (1<<8),
+    /** Location has valid elapsed real time.*/
+    HAS_ELAPSED_REAL_TIME_BIT = (1<<9),
+    /** Location has valid elapsed real time uncertainty.*/
+    HAS_ELAPSED_REAL_TIME_UNC_BIT = (1<<10)
 };
 
 /*Bit mask containing bits from LocationValidityType */
@@ -1753,6 +1793,24 @@ public:
  *
  */
   virtual float getHeadingUncertainty() = 0;
+
+/**
+ * Boot timestamp corresponding to the UTC timestamp for Location fix.
+ *    - Units: Nano-second
+ *
+ * @returns elapsed real time.
+ *
+ */
+  virtual uint64_t getElapsedRealTime() = 0;
+
+/**
+ * Retrieves elapsed real time uncertainty.
+ *    - Units: Nano-second
+ *
+ * @returns elapsed real time uncertainty.
+ *
+ */
+  virtual uint64_t getElapsedRealTimeUncertainty() = 0;
 
 };
 

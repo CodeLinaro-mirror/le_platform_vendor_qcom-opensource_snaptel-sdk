499e13ba77669aef4d3eb87a48f8ab2089f7381a
Faiq Shamass <quic_fshamass@quicinc.com>
2021-12-08 13:16:51 -0800

    telux: Revert to DSD Capability V1
    Revert serving system modem state to use DSD capability V1 instead
    of V2.
    
    Change-Id: Ic02b7cce231439a17bd1691157d4ae17e862e998

--- apps/tests/telsdk_console_app/Data/serving_system/ServingSystemMenu.cpp
+++ apps/tests/telsdk_console_app/Data/serving_system/ServingSystemMenu.cpp
@@ -27,6 +27,42 @@
  *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/*
+ *  Changes from Qualcomm Innovation Center are provided under the following license:
+
+ *  Copyright (c) 2021 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the
+ * disclaimer below) provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 extern "C" {
 #include "unistd.h"
 }
@@ -69,18 +105,15 @@ bool DataServingSystemMenu::init() {
 
     if (addMenuCmds_ == false) {
         addMenuCmds_ = true;
-        std::shared_ptr<ConsoleAppCommand> getDrbStatus =
-            std::make_shared<ConsoleAppCommand>(ConsoleAppCommand("1", "get_drb_status", {},
-            std::bind(&DataServingSystemMenu::getDrbStatus, this, std::placeholders::_1)));
         std::shared_ptr<ConsoleAppCommand> requestServiceStatus =
-            std::make_shared<ConsoleAppCommand>(ConsoleAppCommand("2", "request_service_status", {},
+            std::make_shared<ConsoleAppCommand>(ConsoleAppCommand("1", "request_service_status", {},
             std::bind(&DataServingSystemMenu::requestServiceStatus, this, std::placeholders::_1)));
         std::shared_ptr<ConsoleAppCommand> requestRoamingStatus =
-            std::make_shared<ConsoleAppCommand>(ConsoleAppCommand("3", "request_roaming_status", {},
+            std::make_shared<ConsoleAppCommand>(ConsoleAppCommand("2", "request_roaming_status", {},
             std::bind(&DataServingSystemMenu::requestRoamingStatus, this, std::placeholders::_1)));
 
         std::vector<std::shared_ptr<ConsoleAppCommand>> commandsList = {
-            getDrbStatus, requestServiceStatus, requestRoamingStatus};
+            requestServiceStatus, requestRoamingStatus};
         addCommands(commandsList);
     }
 
@@ -139,24 +172,6 @@ void DataServingSystemMenu::onInitCompleted(telux::common::ServiceStatus status)
     cv_.notify_all();
 }
 
-void DataServingSystemMenu::getDrbStatus(std::vector<std::string> inputCommand) {
-    std::cout << "Get DRB Status\n";
-
-    int slotId = DEFAULT_SLOT_ID;
-    if (telux::common::DeviceConfig::isMultiSimSupported()) {
-        slotId = Utils::getValidSlotId();
-    }
-
-    if (dataServingSystemManagers_.find(static_cast<SlotId>(slotId)) ==
-        dataServingSystemManagers_.end()) {
-        std::cout << "Serving System Manager on SlotId: " << slotId << " is not ready" << std::endl;
-        return;
-    }
-
-    telux::data::DrbStatus stat =
-        dataServingSystemManagers_[static_cast<SlotId>(slotId)]->getDrbStatus();
-    std::cout << "Current Drb Status is : " << DataUtils::drbStatusToString(stat) << std::endl;
-}
 
 void DataServingSystemMenu::requestServiceStatus(std::vector<std::string> inputCommand) {
     std::cout << "Request Service Status\n";

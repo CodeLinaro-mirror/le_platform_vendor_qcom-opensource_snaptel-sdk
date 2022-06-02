5f9489aea771d6cc573a7d0830721e076c695b65
Yujing Gu <quic_yujingg@quicinc.com>
2022-06-02 18:02:58 +0800

    telux: Fix the incorrect subsystem ready time issue
    Issue: The incorrect subsystem ready time displayed due to the jumped system time
    when MDM boot-up
    Fix: Use monotonic clock instead of system wall clock to fix this system time jump
    issue.
    
    CRs-Fixed: 3211498
    Change-Id: I0fbe1fc32de3eaea6f4c1c9a3425997605c53cec

--- apps/tests/telsdk_console_app/Call/CallMenu.cpp
+++ apps/tests/telsdk_console_app/Call/CallMenu.cpp
@@ -29,7 +29,7 @@
 /*
  *  Changes from Qualcomm Innovation Center are provided under the following license:
  *
- *  Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ *  Copyright (c) 2021-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted (subject to the limitations in the
@@ -83,8 +83,8 @@
 
 CallMenu::CallMenu(std::string appName, std::string cursor)
     : ConsoleApp(appName, cursor) {
-   std::chrono::time_point<std::chrono::system_clock> startTime, endTime;
-   startTime = std::chrono::system_clock::now();
+   std::chrono::time_point<std::chrono::steady_clock> startTime, endTime;
+   startTime = std::chrono::steady_clock::now();
    // Get the PhoneFactory and PhoneManager instances.
    auto &phoneFactory = telux::tel::PhoneFactory::getInstance();
    phoneManager_ = phoneFactory.getPhoneManager();
@@ -102,7 +102,7 @@ CallMenu::CallMenu(std::string appName, std::string cursor)
 
    // Exit the application, if SDK is unable to initialize telephony subsystems
    if(subSystemStatus) {
-      endTime = std::chrono::system_clock::now();
+      endTime = std::chrono::steady_clock::now();
       std::chrono::duration<double> elapsedTime = endTime - startTime;
       std::cout << "Elapsed Time for Subsystems to ready : " << elapsedTime.count() << "s"
                 << std::endl;

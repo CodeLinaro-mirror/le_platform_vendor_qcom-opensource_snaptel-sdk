f0ca84aae7ea5b6a9597bb3db5b063c48765d7ef
Shaishav Shroff <quic_shaish@quicinc.com>
2022-08-19 13:22:23 +0530

    tel: Merge below SMS changes.
    Added subsystem initialization APIs for SMS Manager
    Enhance SMS APIs
    fix syntax for calling main() method
    Miscellaneous fix
    Miscellaneous fixes in Sms
    
    CRs-Fixed: 2920348 2746364  3133849
    
    Change-Id: Icda5eb53dee957ba3c45e4b6fe6acd7431595df1

--- apps/tests/telsdk_console_app/Sms/MySmsListener.cpp
+++ apps/tests/telsdk_console_app/Sms/MySmsListener.cpp
@@ -27,6 +27,42 @@
  *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/*
+ *  Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ *  Copyright (c) 2021 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted (subject to the limitations in the
+ *  disclaimer below) provided that the following conditions are met:
+ *
+ *      * Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *
+ *      * Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials provided
+ *        with the distribution.
+ *
+ *      * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *        contributors may be used to endorse or promote products derived
+ *        from this software without specific prior written permission.
+ *
+ *  NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ *  GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ *  HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ *  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ *  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ *  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <iostream>
 
 #include "MySmsListener.hpp"
@@ -37,8 +73,51 @@
 
 void MySmsListener::onIncomingSms(int phoneId, std::shared_ptr<telux::tel::SmsMessage> smsMsg) {
    std::cout << std::endl << std::endl;
-   PRINT_NOTIFICATION << "Received SMS from phone ID " << phoneId << " from: "
-                      << smsMsg->getSender() << "\n message: " << smsMsg->getText() << std::endl;
+   std::shared_ptr<telux::tel::MessagePartInfo> partInfo = smsMsg->getMessagePartInfo();
+   if (partInfo) {
+      PRINT_NOTIFICATION << "Received SMS on phone ID " << phoneId << " from: "
+            << smsMsg->getSender() <<  " to: " << smsMsg->getReceiver()
+            << "\n Message: " << smsMsg->getText()
+            << " \n RefNumber:" << static_cast <int>(partInfo->refNumber) << " NumberOfSegments:"
+            << static_cast <int>(partInfo->numberOfSegments) << " SegmentNumber: "
+            << static_cast <int>(partInfo->segmentNumber)
+            << std::endl;
+   } else {
+      PRINT_NOTIFICATION << "Received SMS on phone ID " << phoneId << " from: "
+            << smsMsg->getSender() <<  " to: " << smsMsg->getReceiver()
+            << "\n Message: " << smsMsg->getText() << std::endl;
+   }
+}
+
+void MySmsListener::onIncomingSms(int phoneId,
+   std::shared_ptr<std::vector<telux::tel::SmsMessage>> msgs) {
+   std::cout << std::endl;
+
+   std::string text = "";
+
+   PRINT_NOTIFICATION << " Consolidated Multipart Message: " << std::endl;
+
+   for (telux::tel::SmsMessage smsMsg : *(msgs.get())) {
+      text = text + smsMsg.getText();
+      std::shared_ptr<telux::tel::MessagePartInfo> partInfo = smsMsg.getMessagePartInfo();
+      std::cout << "\033[1;35mSegment: \033[0m" << static_cast<int>(partInfo->segmentNumber)
+                << "\n SMS Part on phone ID " << phoneId << " from: "
+                << smsMsg.getSender() <<  " to: " << smsMsg.getReceiver()
+                << "\n Message Part: " << smsMsg.getText()
+                << "\n RefNumber:" << static_cast <int>(partInfo->refNumber)
+                << " NumberOfSegments:"
+                << static_cast <int>(partInfo->numberOfSegments) << " SegmentNumber: "
+                << static_cast <int>(partInfo->segmentNumber) << std::endl;
+   }
+   std::cout << "\033[1;35mComplete Message: \033[0m" <<  "\n" << text << std::endl;
+}
+
+void MySmsListener::onDeliveryReport(int phoneId, int msgRef, std::string receiverAddress,
+   telux::common::ErrorCode error) {
+   std::cout << std::endl << std::endl;
+   PRINT_NOTIFICATION << "Received delivery report from phone ID " << phoneId << " with MsgRef: "
+                      << msgRef << " Receiver Address: "<< receiverAddress <<" Error Desc: "
+                      << Utils::getErrorCodeAsString(error) << std::endl;
 }
 
 // Implementation of My SMS callback
@@ -46,6 +125,22 @@ void MySmsCommandCallback::commandResponse(telux::common::ErrorCode error) {
    std::cout << std::endl << std::endl;
    if(error == telux::common::ErrorCode::SUCCESS) {
       PRINT_CB << "sendSmsResponse successfully" << std::endl;
+   } else {
+      PRINT_CB << "sendSmsResponse failed, errorCode: " << static_cast<int>(error)
+               << ", description: " << Utils::getErrorCodeAsString(error) << std::endl;
+   }
+}
+
+// Implementation of My SMS callback
+void MySmsCommandCallback::sendSmsResponse(std::vector<int> msgRefs,
+   telux::common::ErrorCode error) {
+   std::cout << std::endl << std::endl;
+   if(error == telux::common::ErrorCode::SUCCESS) {
+      PRINT_CB << "sendSmsResponse successfully" << std::endl;
+      PRINT_CB << " MsgRefs Size: "<< msgRefs.size() << std::endl;
+      for (int i: msgRefs) {
+         PRINT_CB << " MsgRef : " << i << std::endl;
+      }
    } else {
       PRINT_CB << "sendSmsResponse failed, errorCode: " << static_cast<int>(error)
                << ", description: " << Utils::getErrorCodeAsString(error) << std::endl;

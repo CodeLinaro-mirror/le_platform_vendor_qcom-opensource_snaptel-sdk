7bbf63e7f4a286cf0c030c7570feac68e18ed623
Raxit Raju <quic_raxiraju@quicinc.com>
2022-09-07 06:10:23 -0700

    telux: Add Dgnss Station IDs
    - Add dgnss station Ids as a part of
    detailed and detailed engine reports.
    
    CRs-Fixed: 3268114
    
    Change-Id: I4901291faa7e303742995b51c3bd83ab828ac557

--- apps/tests/location_test_app/MyLocationListener.cpp
+++ apps/tests/location_test_app/MyLocationListener.cpp
@@ -27,6 +27,42 @@
  *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/*
+ *  Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ *  Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted (subject to the limitations in the
+ *  disclaimer below) provided that the following conditions are met:
+ *
+ *      * Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *
+ *      * Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials provided
+ *        with the distribution.
+ *
+ *      * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *        contributors may be used to endorse or promote products derived
+ *        from this software without specific prior written permission.
+ *
+ *  NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ *  GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ *  HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ *  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ *  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ *  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <bitset>
 #include <iostream>
 #include <memory>
@@ -191,6 +227,9 @@ void MyLocationListener::printLocationExValidity(
     }
     if((validityMask & telux::loc::HAS_PROTECT_LEVEL_VERTICAL)) {
       std::cout << "valid protect vertical" << std::endl;
+    }
+    if((validityMask & telux::loc::HAS_DGNSS_STATION_ID)) {
+      std::cout << "valid dgnss station id" << std::endl;
     }
 
 }
@@ -977,6 +1016,18 @@ void MyLocationListener::onCapabilitiesInfo(const telux::loc::LocCapability capa
   LocationUtils::displayCapabilities(capabilityMask);
 }
 
+void MyLocationListener::printDgnssStationIds(std::vector<uint16_t> dgnssStationIds) {
+    if(!dgnssStationIds.empty()) {
+        std::cout << "Dgnss Station IDs : ";
+        for(auto id: dgnssStationIds) {
+            std::cout << id << " ";
+        }
+        std::cout << std::endl;
+    } else {
+        std::cout << "No Dgnss Station Id is present\n";
+    }
+}
+
 void MyLocationListener::onBasicLocationUpdate(
    const std::shared_ptr<telux::loc::ILocationInfoBase> &locationInfo) {
    if(!isBasicReportFlagEnabled_) {
@@ -1127,6 +1178,7 @@ void MyLocationListener::onDetailedLocationUpdate(
        locationInfo->getProtectionLevelCrossTrack() << std::endl;
    std::cout << "Protection level vertical : " <<
        locationInfo->getProtectionLevelVertical() << std::endl;
+   printDgnssStationIds(locationInfo->getDgnssStationIds());
    std::cout << "*************************************************************" << std::endl;
 }
 
@@ -1247,6 +1299,7 @@ void MyLocationListener::onDetailedEngineLocationUpdate(
          locationInfo->getProtectionLevelCrossTrack() << std::endl;
      std::cout << "Protection level vertical : " <<
          locationInfo->getProtectionLevelVertical() << std::endl;
+     printDgnssStationIds(locationInfo->getDgnssStationIds());
      std::cout << "*************************************************************" << std::endl;
     }
 }

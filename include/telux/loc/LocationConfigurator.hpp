1907f785c6653b198579b8d664f49cff32a801d6
Raxit Raju <quic_raxiraju@quicinc.com>
2022-09-13 03:36:39 -0700

    telux: Add datum type support.
    - Added Nmea Datum Type support for Nmea Sentences
      in location.
    
    CRs-Fixed: 3272219
    
    Change-Id: Iccb459b394665ab0656a907c012f59a6c723fa70

--- include/telux/loc/LocationConfigurator.hpp
+++ include/telux/loc/LocationConfigurator.hpp
@@ -27,6 +27,42 @@
  *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/*
+ *  Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ *  Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted (subject to the limitations in the
+ *  disclaimer below) provided that the following conditions are met:
+ *
+ *      * Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *
+ *      * Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials provided
+ *        with the distribution.
+ *
+ *      * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *        contributors may be used to endorse or promote products derived
+ *        from this software without specific prior written permission.
+ *
+ *  NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ *  GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ *  HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ *  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ *  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ *  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 /**
  * @file       LocationConfigurator.hpp
  * @brief      Location configurator provides APIs for enabling/disabling
@@ -484,6 +520,36 @@ using GetRobustLocationCallback = std::function<void(const telux::loc::
 
   virtual telux::common::Status configureNmeaTypes(const NmeaSentenceConfig nmeaType,
       telux::common::ResponseCallback callback = nullptr) = 0;
+
+/**
+ * This API is used to configure the NMEA sentences that the clients will receive via
+ * @ref ILocationManager::startDetailedReports or
+ * @ref ILocationManager::startDetailedEngineReports.
+ * Without prior invocation to this API, all NMEA sentences supported in the system will get
+ * generated and delivered to all the clients that register to receive NMEA sentences.
+ * The NMEA sentence type configuration is common across all clients and updating it will affect
+ * all clients.
+ *
+ * Please note that for the NMEA datum type request to be successful,
+ * the nmea provider configuration in the GPS configuration file
+ * should be set to application processor.
+ *
+ * This API call is not incremental and the new NMEA configuration will completely overwrite the
+ * previous call to this API.
+ *
+ * @param [in] configParams - Configuration Parameters for Nmea on the device.
+ *
+ * @param [in] callback - Optional callback to get the response of configureNmea.
+ *
+ * @returns Status of configureNmea i.e. success or suitable status code.
+ *
+ * @note Eval: This is a new API and is being evaluated. It is subject to change and could
+ *             break backwards compatibility.
+ *
+ */
+
+  virtual telux::common::Status configureNmea(const NmeaConfig configParams,
+    telux::common::ResponseCallback callback = nullptr) = 0;
 
 /**
  * This API is used to instruct the specified engine to use the provided integrity risk level for

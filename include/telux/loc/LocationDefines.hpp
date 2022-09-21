44f4757ff56e6881c4bca6944390fdc6a33f2026
Raxit Raju <quic_raxiraju@quicinc.com>
2022-09-21 00:16:27 -0700

    telux: Adding Propagated bit in Location
    - Adding Propagated bit as a technology type
    for PVT Reports.
    
    CRs-Fixed: 3291537
    
    Change-Id: I69cd35cbd821bb1b0feb2092d8a3d9e37735d306

--- include/telux/loc/LocationDefines.hpp
+++ include/telux/loc/LocationDefines.hpp
@@ -27,6 +27,42 @@
  *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/*
+ *  Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ *  Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted (subject to the limitations in the
+ *  disclaimer below) provided that the following conditions are met:
+ *
+ *      * Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *
+ *      * Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials provided
+ *        with the distribution.
+ *
+ *      * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *        contributors may be used to endorse or promote products derived
+ *        from this software without specific prior written permission.
+ *
+ *  NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ *  GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ *  HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ *  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ *  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ *  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 /**
  * @file       LocationDefines.hpp
  *
@@ -202,19 +238,40 @@ enum class SVInfoAvailability {
 };
 
 /**
- * Specifies which position technology was used.
+ * Specifies which position technology was used to generate location
+ * information in the @ref ILocationInfoEx.
  */
 enum GnssPositionTechType {
+  /** Technology used to generate location info
+   *  is unknown.*/
   GNSS_DEFAULT = 0,
+  /** Satellites-based technology was used to generate
+   *  location info.*/
   GNSS_SATELLITE = (1 << 0),
+  /** Cell towers were used to generate location info.*/
   GNSS_CELLID = (1 << 1),
+  /** Wi-Fi access points were used to generate location info.*/
   GNSS_WIFI = (1 << 2),
+  /** Sensors were used to generate location info.*/
   GNSS_SENSORS = (1 << 3),
+  /**  Reference location was used to generate location info.*/
   GNSS_REFERENCE_LOCATION = (1 << 4),
+  /** Coarse position injected into the location engine was used to
+   *  generate location info.*/
   GNSS_INJECTED_COARSE_POSITION= (1 << 5),
+  /** AFLT was used to generate location info.*/
   GNSS_AFLT = (1 << 6),
+  /** GNSS and network-provided measurements were used to generate
+   *  location info.*/
   GNSS_HYBRID = (1 << 7),
-  GNSS_PPE = (1 << 8)
+  /** Precise position engine was used to generate location info.*/
+  GNSS_PPE = (1 << 8),
+  /** Location was calculated using Vehicular data. */
+  GNSS_VEHICLE = (1 << 9),
+  /** Location was calculated using Visual data. */
+  GNSS_VISUAL = (1 << 10),
+  /** Location was calculated using Propagation logic, which uses cached measurements. */
+  GNSS_PROPAGATED = (1 << 11),
 };
 
 /*Bit mask containing bits from GnssPositionTechType */
@@ -560,6 +617,8 @@ enum LocationTechnologyType {
   LOC_VEH = (1 << 9),
   /** Location was calculated using Visual data. */
   LOC_VIS = (1 << 10),
+  /** Location was calculated using Propagation logic, which uses cached measurements. */
+  LOC_PROPAGATED = (1 << 11),
 };
 
 /*Bit mask containing bits from LocationTechnologyType */

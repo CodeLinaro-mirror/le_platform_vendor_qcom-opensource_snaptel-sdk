64a56d8486d486473a5a4874014ad90d01edb817
Rishi Gupta <quic_rishgupt@quicinc.com>
2022-10-10 20:42:23 -0700

    audio: make console output available immediately
    Asynchronous response received to previously sent
    command may not appear on console immediately giving
    impression that the command is not executed.
    
    Therefore flush the output on console as soon as it
    is received.
    
    Change-Id: Iadcd3f78a0dfe3335df8aebefaf609141e66f934

--- apps/tests/audio_console_app/AudioClient.cpp
+++ apps/tests/audio_console_app/AudioClient.cpp
@@ -513,8 +513,8 @@ void AudioClient::getStreamDevice(StreamType streamType, SlotId slotId) {
                 devices_ = devices;
                 p.set_value(true);
             } else {
-                p.set_value(false);
                 std::cout << "Failed to get stream device" << std::endl;
+                p.set_value(false);
             }
         });
         if(status == telux::common::Status::SUCCESS) {
@@ -526,7 +526,7 @@ void AudioClient::getStreamDevice(StreamType streamType, SlotId slotId) {
         if (p.get_future().get()) {
             for (auto deviceType : devices_) {
                 std::string deviceName;
-                std::cout << "Device Type"  << (static_cast<uint32_t>(deviceType)) << std::endl;
+                std::cout << "Device type: "  << (static_cast<uint32_t>(deviceType)) << std::endl;
                 std::cout.flush();
             }
         }
@@ -604,8 +604,8 @@ void AudioClient::getVolume(StreamType streamType, SlotId slotId) {
                 vol = volume;
                 p.set_value(true);
             } else {
-                p.set_value(false);
                 std::cout << "Failed to set stream device" << std::endl;
+                p.set_value(false);
             }
         });
         if(status == telux::common::Status::SUCCESS) {
@@ -616,7 +616,7 @@ void AudioClient::getVolume(StreamType streamType, SlotId slotId) {
 
         if (p.get_future().get()) {
             for (auto channelVolume : vol.volume) {
-                std::cout << "volume: "<< channelVolume.vol << std::endl;
+                std::cout << "Volume: "<< channelVolume.vol << std::endl;
                 std::cout.flush();
             }
         }
@@ -700,8 +700,8 @@ void AudioClient::getMute(StreamType streamType, SlotId slotId) {
                 mute_ = mute;
                 p.set_value(true);
             } else {
-                p.set_value(false);
                 std::cout << "Failed to get mute" << std::endl;
+                p.set_value(false);
             }
         });
         if(status == telux::common::Status::SUCCESS) {
@@ -716,7 +716,7 @@ void AudioClient::getMute(StreamType streamType, SlotId slotId) {
             } else {
                 muteStatus = "Unmuted";
             }
-            std::cout << "Mute Status: " << muteStatus << std::endl;
+            std::cout << "Mute status: " << muteStatus << std::endl;
             std::cout.flush();
         }
     } else {

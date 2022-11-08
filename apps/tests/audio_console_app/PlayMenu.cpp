f8edff7818f8cdcd31df2c951f1c46800ec4bc13
Amisha Singh <quic_amissing@quicinc.com>
2022-11-08 18:30:26 +0530

    audio : fix for console app crash multiple start play
    Audio console app crashes when multiple start play
    triggered for same playback stream.
    
    To prevent this added check to determine if a file
    is being played. If played, disallow playing again
    until 1st play finishes.
    
    CRs-Fixed: 3319665
    
    Change-Id: Ie3d42e78607f8147083f2c2b8bb5e6da4262b7b1

--- apps/tests/audio_console_app/PlayMenu.cpp
+++ apps/tests/audio_console_app/PlayMenu.cpp
@@ -76,6 +76,7 @@ PlayMenu::PlayMenu(std::string appName, std::string cursor,
    audioClient_(audioClient) {
     pipeLineEmpty_ = true;
     ready_ = false;
+    playInProgress_ = false;
 
 }
 
@@ -247,9 +248,15 @@ void PlayMenu::setMute(std::vector<std::string> userInput) {
 
 void PlayMenu::startPlay(std::vector<std::string> userInput) {
     if(audioPlayStream_) {
-        audioClient_->getPlayConfig(filePath_, playFormat_);
-        std::thread playThread(&PlayMenu::play, this);
-        runningThreads_.emplace_back(std::move(playThread));
+        // Check if a file is already being played.
+        if(!playInProgress_) {
+            audioClient_->getPlayConfig(filePath_, playFormat_);
+            std::thread playThread(&PlayMenu::play, this);
+            runningThreads_.emplace_back(std::move(playThread));
+            playInProgress_ = true;
+        } else {
+            std::cout << "File Play in progress please wait" << std::endl;
+        }
     } else {
         std::cout << "No running Play session please create one" << std::endl;
     }
@@ -405,6 +412,7 @@ void PlayMenu::play() {
         std::cout << "Play Stopped" << std::endl;
     }
     playStatus_ = false;
+    playInProgress_ = false;
 }
 
 void PlayMenu::onReadyForWrite() {

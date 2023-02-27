3ea70906044e7a681b3b8cf848ac3f8db7e2d773
Yujing Gu <quic_yujingg@quicinc.com>
2023-02-27 11:00:15 +0800

    tel: Added new API to get IMS service information.
    - Added new API to get IMS service information: voip and sms service status.
    
    CRs-Fixed: 3419535
    Change-Id: Ic1bf985459cf07129509ff75ec5e298ea0d28164

--- apps/tests/telsdk_console_app/ImsServingSystem/ImsServingSystemMenu.cpp
+++ apps/tests/telsdk_console_app/ImsServingSystem/ImsServingSystemMenu.cpp
@@ -27,6 +27,42 @@
  *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/*
+ *  Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ *  Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the
+ * disclaimer below) provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 /**
  * ImsServingSystemMenu provides menu options to invoke IMS Serving System APIs
  * such as requestImsRegStatus.
@@ -113,9 +149,13 @@ void ImsServingSystemMenu::init() {
         = std::make_shared<ConsoleAppCommand>(ConsoleAppCommand("1", "Get_Registration_Status",
         {}, std::bind(&ImsServingSystemMenu::requestImsRegStatus, this,
         std::placeholders::_1)));
+    std::shared_ptr<ConsoleAppCommand> queryServiceStatusOverImsCommand
+        = std::make_shared<ConsoleAppCommand>(ConsoleAppCommand("2", "Get_Service_Status",
+        {}, std::bind(&ImsServingSystemMenu::requestServiceStatusOverIms, this,
+        std::placeholders::_1)));
 
     std::vector<std::shared_ptr<ConsoleAppCommand>> commandsListImsServSysMenu
-        = { queryImsRegStateCommand };
+        = { queryImsRegStateCommand, queryServiceStatusOverImsCommand };
 
     addCommands(commandsListImsServSysMenu);
     ConsoleApp::displayMenu();
@@ -136,6 +176,30 @@ void ImsServingSystemMenu::requestImsRegStatus(std::vector<std::string> userInpu
             std::cout << "IMS registration status request sent successfully " << std::endl;
         } else {
             std::cout << "ERROR - Failed to send registration status request,"
+                    << "Status:" << static_cast<int>(status) << std::endl;
+            Utils::printStatus(status);
+        }
+    } else {
+        std::cout << "ERROR - ImsServingSystemManger on slot " << slotId
+            << " is null" << std::endl;
+    }
+}
+
+void ImsServingSystemMenu::requestServiceStatusOverIms(std::vector<std::string> userInput) {
+    SlotId slotId = SlotId::DEFAULT_SLOT_ID;
+    if (slotCount_ > DEFAULT_NUM_SLOTS) {
+        slotId = static_cast<SlotId>(Utils::getValidSlotId());
+    }
+
+    if (imsServingSystemMgrs_[slotId]) {
+        auto response = [this, slotId](telux::tel::ImsServiceInfo service, ErrorCode error) {
+            MyImsServSysCallback::imsServiceStatusResponse(slotId, service, error);
+        };
+        Status status = imsServingSystemMgrs_[slotId]->requestServiceInfo(response);
+        if (status == Status::SUCCESS) {
+            std::cout << "IMS service status request sent successfully " << std::endl;
+        } else {
+            std::cout << "ERROR - Failed to send service status request,"
                     << "Status:" << static_cast<int>(status) << std::endl;
             Utils::printStatus(status);
         }

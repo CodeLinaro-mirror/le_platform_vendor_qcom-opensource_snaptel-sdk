573a4e29d79acc701334d439f90d84cec307152d
Rakesh T <quic_raket@quicinc.com>
2023-03-16 13:17:23 +0530

    telux: Fix KW issues.
    CRs-Fixed: 3408487
    
    Change-Id: I14d0097630c3910a2a4fab4899960d2aec5baa88

--- apps/reference/rits/src/qMessenger/GeoNetRouter/GeoNetRouterImpl.cpp
+++ apps/reference/rits/src/qMessenger/GeoNetRouter/GeoNetRouterImpl.cpp
@@ -26,6 +26,41 @@
  *  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+ /*
+ *  Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ *  Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted (subject to the limitations in the
+ *  disclaimer below) provided that the following conditions are met:
+ *
+ *      * Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *
+ *      * Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials provided
+ *        with the distribution.
+ *
+ *      * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *        contributors may be used to endorse or promote products derived
+ *        from this software without specific prior written permission.
+ *
+ *  NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ *  GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ *  HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ *  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ *  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ *  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 /**
  * @file GeoNetRouterImpl.cpp
  * @brief implementation of GeoNetwork router.
@@ -1351,6 +1386,10 @@ namespace gn {
                 }
                 p = (uint8_t *)abuf_push(&mc->abuf, sizeof(gn_guc_hdr_t) + 1);
                 BufLen += sizeof(gn_guc_hdr_t) + 1;
+                if (!p) {
+                    std::cerr << "GN_PACKET_TYPE_GEOUNICAST req is null" << std::endl;
+                    return retVal;
+                }
                 *p = 0x03;
                 retVal = TransmitGUC(p, BufLen, data, txcb);
                 break;
@@ -1362,6 +1401,11 @@ namespace gn {
                 }
                 p = (uint8_t *)abuf_push(&mc->abuf, sizeof(gn_gbc_gac_hdr_t) + 1);
                 BufLen += sizeof(gn_gbc_gac_hdr_t) + 1;
+                if (!p) {
+                    std::cerr << "GN_PACKET_TYPE_GEOANYCAST| GN_PACKET_TYPE_GEOBROADCAST \
+                                  req is null" << std::endl;
+                    return retVal;
+                }
                 *p = 0x03;
                 retVal = TransmitGBCGAC(p, BufLen, data, txcb);
             case PacketType::GN_PCCKET_TYPE_TSB:
@@ -1372,6 +1416,10 @@ namespace gn {
                     }
                     p = (uint8_t *)abuf_push(&mc->abuf, sizeof(gn_tsb_hdr_t) + 1);
                     BufLen += sizeof(gn_tsb_hdr_t) + 1;
+                    if (!p) {
+                        std::cerr << "GN_PCCKET_TYPE_TSB req is null" << std::endl;
+                        return retVal;
+                    }
                     *p = 0x03;
                     retVal = TransmitTSB(p, BufLen, data, txcb);
                 } else {
@@ -1381,6 +1429,10 @@ namespace gn {
                     }
                     p = (uint8_t *)abuf_push(&mc->abuf, sizeof(gn_shb_hdr_t) + 1);
                     BufLen += sizeof(gn_shb_hdr_t) + 1;
+                    if (!p) {
+                        std::cerr << "GN_PCCKET_TYPE_TSB req data is null" << std::endl;
+                        return retVal;
+                    }
                     *p = 0x03;
                     retVal = TransmitSHB(p, BufLen, data, txcb);
                 }

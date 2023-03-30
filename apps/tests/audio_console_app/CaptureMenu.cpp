712b24e4738c0f73772c9db035988f611abb65c9
Suraj Hiriyur Sudarshan <quic_hiriyurs@quicinc.com>
2023-03-30 12:55:12 +0530

    audio: stop capture incase of failure.
    If an error is recieved by server during
    read operation, stop sending buffers
    and close file to exit record process.
    
    Change-Id: If5c1c8238c1699ae437dddd65232d18a0068fbc8

--- apps/tests/audio_console_app/CaptureMenu.cpp
+++ apps/tests/audio_console_app/CaptureMenu.cpp
@@ -27,6 +27,42 @@
  *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/*
+ *  Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ *  Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the
+ * disclaimer below) provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <chrono>
 #include <iostream>
 #include <queue>
@@ -43,6 +79,7 @@ CaptureMenu::CaptureMenu(std::string appName, std::string cursor,
    audioClient_(audioClient) {
        captureStatus_ = false;
        ready_ = false;
+       readFail_ = false;
 }
 
 CaptureMenu::~CaptureMenu() {
@@ -286,20 +323,37 @@ void CaptureMenu::record() {
             if(status != telux::common::Status::SUCCESS) {
                 std::cout << "read() failed with error" << static_cast<unsigned int>(status)
                 <<std::endl;
+                streamBuffer->reset();
+                freeBuffers_.push(streamBuffer);
+                readFail_ = true;
+                goto exit;
             }
         } else {
             cv_.wait(lock);
+            if(readFail_) {
+                goto exit;
+            }
         }
     }
-    int waitTime = (8*(streamBuffer->getMaxSize())*1000)/
+
+    exit:
+    if(readFail_) {
+        while(freeBuffers_.size() != TOTAL_BUFFERS) {
+            cv_.wait(lock);
+        }
+        std::cout << "File Recording Failed" <<std::endl;
+    } else {
+        int waitTime = (8*(streamBuffer->getMaxSize())*1000)/
                         (sampleRate*numChannels*BITS_PER_SAMPLE);
-    waitTime = waitTime+100;
-    while(freeBuffers_.size() != TOTAL_BUFFERS && ready_) {
-        cv_.wait_for(lock, std::chrono::milliseconds(waitTime));
+        waitTime = waitTime+100;
+        while(freeBuffers_.size() != TOTAL_BUFFERS && ready_) {
+            cv_.wait_for(lock, std::chrono::milliseconds(waitTime));
+        }
+        std::cout << "File Recorded SuccessFully" <<std::endl;
     }
+
     fflush(file_);
     fclose(file_);
-    std::cout << "File Recorded SuccessFully" <<std::endl;
     captureStatus_ = false;
 }
 
@@ -310,6 +364,7 @@ void CaptureMenu::readCallback(std::shared_ptr<telux::audio::IStreamBuffer> buff
     if (error != telux::common::ErrorCode::SUCCESS) {
         std::cout << "read() returned with error " << static_cast<unsigned int>(error)
             << std::endl;
+        readFail_ = true;
     } else {
         uint32_t size = buffer->getDataSize();
         bytesWrittenToFile = fwrite(buffer->getRawBuffer(),1,size,file_);

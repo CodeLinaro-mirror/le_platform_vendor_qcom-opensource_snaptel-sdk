73df1fb236d28a7e5e6d82859e5bb48321791dd6
Amisha Singh <quic_amissing@quicinc.com>
2023-04-04 19:42:59 +0530

    audio: updated console output for voice menu
    Earlier the console output prints were not indicating
    that existing streams are cleared post SSR and currently
    no stream exists.
    
    Updated the print statements to indicate that no voice
    stream exists
    
    CRs-Fixed: 3419816

--- apps/tests/audio_console_app/VoiceMenu.hpp
+++ apps/tests/audio_console_app/VoiceMenu.hpp
@@ -27,6 +27,42 @@
  *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/*
+ *  Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ *  Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted (subject to the limitations in the
+ *  disclaimer below) provided that the following conditions are met:
+ *
+ *      * Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *
+ *      * Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials provided
+ *        with the distribution.
+ *
+ *      * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *        contributors may be used to endorse or promote products derived
+ *        from this software without specific prior written permission.
+ *
+ *  NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ *  GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ *  HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ *  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ *  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ *  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #ifndef VOICEMENU_HPP
 #define VOICEMENU_HPP
 
@@ -39,8 +75,8 @@
 
 class VoiceMenu : public ConsoleApp,
                   public telux::audio::IVoiceListener,
-                  public std::enable_shared_from_this<VoiceMenu>{
-public:
+                  public std::enable_shared_from_this<VoiceMenu> {
+ public:
     VoiceMenu(std::string appName, std::string cursor);
     ~VoiceMenu();
     void init();
@@ -48,7 +84,7 @@ public:
     void cleanup();
     virtual void onDtmfToneDetection(DtmfTone dtmfTone) override;
 
-private:
+ private:
     void createStream(std::vector<std::string> userInput);
     void deleteStream(std::vector<std::string> userInput);
     void getDevice(std::vector<std::string> userInput);
@@ -64,7 +100,9 @@ private:
     void registerListener(std::vector<std::string> userInput);
     void deRegisterListener(std::vector<std::string> userInput);
     void changeSlotId();
-    void setActiveSession(SlotId slotId);
+    void deleteActiveSession(SlotId slotId);
+    Status createActiveSession(SlotId slotId);
+    Status setActiveSession(SlotId slotId);
 
     std::shared_ptr<VoiceSession> activeSession_;
     std::mutex mutex_;

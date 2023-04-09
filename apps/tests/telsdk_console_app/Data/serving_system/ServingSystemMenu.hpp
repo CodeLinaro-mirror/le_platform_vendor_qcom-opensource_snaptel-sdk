f45b7de25dcd2d76e2433325b9692ffe95bae67d
Faiq Shamass <quic_fshamass@quicinc.com>
2023-04-08 21:09:15 -0700

    data: Adding Switch to Dormancy Request
    Added API to request modem switch to dormant state to expedite
    re-selection to higher RAT when available if automatic switch is
    not supported by network operator
    
    Change-Id: If86a3273f9e1231c530ad228f83e32b8cec80204

--- apps/tests/telsdk_console_app/Data/serving_system/ServingSystemMenu.hpp
+++ apps/tests/telsdk_console_app/Data/serving_system/ServingSystemMenu.hpp
@@ -29,8 +29,7 @@
 
 /*
  *  Changes from Qualcomm Innovation Center are provided under the following license:
-
- *  Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ *  Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted (subject to the limitations in the
@@ -103,6 +102,7 @@ public:
     void requestServiceStatus(std::vector<std::string> inputCommand);
     void requestRoamingStatus(std::vector<std::string> inputCommand);
     void requestNrIconType(std::vector<std::string> inputCommand);
+    void makeDormant(std::vector<std::string> inputCommand);
 
     ~DataServingSystemMenu();
 private:

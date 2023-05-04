e13cc1bc50c8a2bb0f7a1f665924ff912c9e6a80
Siva Kumar Mellemputi <quic_smellemp@quicinc.com>
2023-05-04 12:51:44 +0530

    tel: Fix to MSD update issue in eCall reference application
    Issue:
    - When an MO eCall is terminated before the MSD transmission is
      complete, modem performs a redial.
      After the redial, ecall app is printing some errors on the
      console about failures in updating the MSD.
    - Upon the redial, ecall reference app is not updating the phone ID
      which will be used during MSD update. So SDK API is returning
      an error since an invalid phone ID was used.
    
    Fix:
    - Fix to update the phone ID when the redial is detected, so that
      MSD update happens on that valid phone ID.
    
    CRs-Fixed: 3487479
    
    Change-Id: I37894bdbfe2b17e94fc54518fcd349c77dbe2d4a
    (cherry picked from commit 6a3dbb9c167ba64e8468500b145c6569b364cc6e)

--- apps/reference/ecall_app/ECallManager.cpp
+++ apps/reference/ecall_app/ECallManager.cpp
@@ -185,7 +185,6 @@ telux::common::Status ECallManager::answerCall(int phoneId) {
             return telux::common::Status::FAILED;
         }
     }
-    phoneId_ = phoneId;
     setup(phoneId);
     auto status = telClient_->answer(phoneId_, shared_from_this());
     if(status != telux::common::Status::SUCCESS) {
@@ -297,6 +296,7 @@ telux::common::Status ECallManager::getEncodedOptionalAdditionalDataContent() {
  * that are required for an eCall
  */
 void ECallManager::setup(int phoneId) {
+    phoneId_ = phoneId;
     // Start voice session
     if(!audioClient_) {
         std::cout << CLIENT_NAME << "Invalid Audio Client, cannot establish voice conversation"

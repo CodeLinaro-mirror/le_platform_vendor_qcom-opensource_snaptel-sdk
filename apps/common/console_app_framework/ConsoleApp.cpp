3e1c4f6243f849451f89f3cb7a7af3a7fc4f30d8
Amisha Singh <quic_amissing@quicinc.com>
2023-09-05 20:25:46 +0530

    common: Handle error states for cin
    When user inputs are piped to any console app, the cin stream
    of the console enters failed state after all the inputs are
    written on the write end of the pipe and post this only EOF
    is being written. Since, piping redirects the inputs to cin
    once EOF is reached cin stream always reads EOF and eofbit is
    set for cin. When we again try to read, the failbit is also set
    indicating read operation failed to complete successfully.
    
    Getline extraction stops if the end of file is reached in stream
    or delimitation character is found. When cin's eofbit is set and
    we try to read using getline the extraction stop with 0 bytes read.
    This results in an infinte loop.
    
    To avoid this, added cin stream-state check to exit gracefully
    when cin receives eof or enters a bad state.
    
    CRs-Fixed: 3575047
    Change-Id: Id9e08d728bd519e1eeb46c9e80b086862ea8312f

--- apps/common/console_app_framework/ConsoleApp.cpp
+++ apps/common/console_app_framework/ConsoleApp.cpp
@@ -27,6 +27,40 @@
  *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/*
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the
+ * disclaimer below) provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <algorithm>
 #include <iostream>
 #include <iomanip>
@@ -82,7 +116,14 @@ std::vector<std::string> ConsoleApp::readCommand() {
    ConsoleApp::displayCursor();
    // input string
    std::string command;
+
    std::getline(std::cin, command);
+   if (std::cin.fail() || std::cin.bad() || std::cin.eof()) {
+      std::cin.clear();
+      std::cin.ignore();
+      std::cout << "\ncin has entered bad state" << std::endl;
+      command = "quit";
+   }
 
    // separate input string based on whitespace
    std::istringstream iss(command);

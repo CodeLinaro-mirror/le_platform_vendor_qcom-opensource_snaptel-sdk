ad23a13e071361eed6bf89842a42c014e07414e8
Abhijeet Jagdale <quic_ajagdale@quicinc.com>
2023-10-04 19:58:47 +0530

    power: update config parser for telux_power_refd
    - issue: Commenting out CAN trigger from telux_power_refd.conf not
             taking affect.
    - fix: Handle negative scenario in case trigger configuration is
           not available.
           Hashtag '#' in start of the line consider as comment and
           don’t use such comment as configuration.
    
    CRs-Fixed: 3598703
    Change-Id: I0455f5687e8b3c95629fb3d3bf02931f6fecff64

--- apps/reference/power_ref_app/trigger/CAN/CANTrigger.cpp
+++ apps/reference/power_ref_app/trigger/CAN/CANTrigger.cpp
@@ -158,22 +158,28 @@ std::shared_ptr<CANTrigger> CANTrigger::getInstance(std::shared_ptr<EventManager
 }
 
 bool CANTrigger::loadTrigger() {
-   LOG(DEBUG, __FUNCTION__);
-   uint32_t triggerSuspend, triggerResume, triggerShutdown;
-   triggerSuspend = std::stoul(config_->getValue("CAN_TRIGGER", TRIGGER_SUSPEND), nullptr, 16);
-   triggerResume = std::stoul(config_->getValue("CAN_TRIGGER", TRIGGER_RESUME), nullptr, 16);
-   triggerShutdown = std::stoul(config_->getValue("CAN_TRIGGER", TRIGGER_SHUTDOWN), nullptr, 16);
-
-   if (triggerSuspend == triggerResume || triggerSuspend == triggerShutdown ||
-       triggerResume == triggerShutdown) {
-      LOG(ERROR, __FUNCTION__, " Error : same trigger for multiple state");
-      return false;
-   }
-
-
-   triggers_.insert({triggerSuspend, {TcuActivityState::SUSPEND, 0}});
-   triggers_.insert({triggerResume, {TcuActivityState::RESUME, 0}});
-   triggers_.insert({triggerShutdown, {TcuActivityState::SHUTDOWN, 0}});
-
-   return true;
+    LOG(DEBUG, __FUNCTION__);
+    std::map<std::string, TcuActivityState> expectedTrigger{
+        {TRIGGER_SUSPEND, TcuActivityState::SUSPEND},
+        {TRIGGER_RESUME, TcuActivityState::RESUME},
+        {TRIGGER_SHUTDOWN, TcuActivityState::SHUTDOWN}};
+    try {
+        std::string configText = "";
+        uint32_t triggerCANId = 0;
+        for (auto itr = expectedTrigger.begin(); itr != expectedTrigger.end(); ++itr) {
+            configText = config_->getValue("CAN_TRIGGER", itr->first);
+            if (!configText.empty()) {
+                triggerCANId = std::stoul(configText, nullptr, 16);
+                if (triggers_.find(triggerCANId) != triggers_.end()) {
+                    LOG(ERROR, __FUNCTION__, " Error : same trigger for multiple state");
+                    return false;
+                }
+                triggers_.insert({triggerCANId, {itr->second, 0}});
+            }
+        }
+    } catch (const std::invalid_argument& ia) {
+        LOG(ERROR, __FUNCTION__, " Error : invalid argument");
+        return false;
+    }
+    return true;
 }

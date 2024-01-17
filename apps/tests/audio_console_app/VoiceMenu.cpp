05efaa4daf50ee0b1f9da2309a384dfb41fce5eb
Rishi Gupta <quic_rishgupt@quicinc.com>
2024-01-17 09:18:42 -0800

    audio: emit correct log for non-existent session
    The else clause is entered when there is no
    active session. Log should indicate this instead
    of service status. This commit fixes this.
    
    CRs-Fixed: 3708645
    Change-Id: I8f0d69b6e50c8e436fe7b14ee42bad13f5e400af

--- apps/tests/audio_console_app/VoiceMenu.cpp
+++ apps/tests/audio_console_app/VoiceMenu.cpp
@@ -30,7 +30,7 @@
 /*
  *  Changes from Qualcomm Innovation Center are provided under the following license:
  *
- *  Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ *  Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted (subject to the limitations in the
@@ -163,7 +163,8 @@ void VoiceMenu::createStream(std::vector<std::string> userInput) {
                     std::cout << "Stream creation failed on slotId : " << slotId_ << std::endl;
                 }
             } else {
-                std::cout << "Audio Service UNAVAILABLE" << std::endl;
+                std::cout << "No running voice session for slotId : " << slotId_
+                      << ", please create one" << std::endl;
                 mutex_.unlock();
             }
         }

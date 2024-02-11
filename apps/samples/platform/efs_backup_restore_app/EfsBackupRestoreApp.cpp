2a7af9ec4b9e6293c5ac9de30dabd952dfc768ed
Rishi Gupta <quic_rishgupt@quicinc.com>
2024-02-10 18:33:43 -0800

    platform: consolidated sample apps improvements
    CMake changes:
    - Use target_include_directories command to
      include required directories.
    - Bump up version from 2.8.9 to 3.10.2
    
    Code changes:
    - Rework whole app for overall better quality.
    - Show functional flow for use case and telsdk API usage
      distinctly.
    - Explicitly specify what is the purpose of the application.
    - Initialize the local variables as needed.
    - Categories C, CPP and telsdk specific headers inclusion.
    - If error occurs, emit appropriate log, bail out immediately.
    - Remove calling getServiceStatus() as initialization callback
      passed to getFooManager() indicates whether the service is
      available or unavailable.
    - Replace auto keyword with exact class name to make it more
      easy for user to understand the details of the telsdk API.
    - Separate out returning application specific error code and
      telsdk error code.
    - Reduce verbosity as applicable for code and logging.
    - Remove global variables reducing their scope.
    
    CRs-Fixed: 3813667
    Change-Id: I6de9182376e8f6242a5ef4bd861d07040905f5dd

--- apps/samples/platform/efs_backup_restore_app/EfsBackupRestoreApp.cpp
+++ apps/samples/platform/efs_backup_restore_app/EfsBackupRestoreApp.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2021 Qualcomm Innovation Center, Inc. All rights reserved.
+ *  Copyright (c) 2021,2024 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted (subject to the limitations in the
@@ -32,119 +32,155 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-
-/**
- * This sample application demonstrates the use of the filesystem manager (IFsManager) to start
- * EFS backup and receive filesystem events like start or end of EFS restore and backup events
+/*
+ * This application demonstrates how to use the filesystem manager to start
+ * EFS backup and receive filesystem events like start or end of the EFS backup
+ * events. The steps are as follows:
+ *
+ * 1. Get a PlatformFactory instance.
+ * 2. Get a IFsManager instance from the PlatformFactory.
+ * 3. Wait for the file system service to become available.
+ * 4. Register a listener that will receive EFS backup events.
+ * 5. Request to trigger an EFS backup.
+ * 6. Receive status of EFS backup in the listener.
+ * 7. Finally, deregister the listener.
+ *
+ * Usage:
+ * # ./efs_backup_restore_app
  */
 
-#include <future>
-#include <getopt.h>
+#include <errno.h>
+
 #include <iostream>
-#include <condition_variable>
+#include <memory>
+#include <cstdlib>
+#include <future>
 
-#include "../../../common/utils/Utils.hpp"
+#include <telux/common/CommonDefines.hpp>
 #include <telux/platform/PlatformFactory.hpp>
+#include <telux/platform/FsManager.hpp>
+#include <telux/platform/FsListener.hpp>
 
-#define PRINT_NOTIFICATION std::cout << std::endl << "\033[1;35mNOTIFICATION: \033[0m"
-
-class EfsEventListener : public telux::platform::IFsListener {
+class EFSBackupRestore : public telux::platform::IFsListener,
+                         public std::enable_shared_from_this<EFSBackupRestore> {
  public:
-    // [6] Receive service status notifications
-    virtual void onServiceStatusChange(telux::common::ServiceStatus serviceStatus) override {
-        PRINT_NOTIFICATION << "Filesystem service status: ";
-        std::string status;
-        switch (serviceStatus) {
-            case telux::common::ServiceStatus::SERVICE_AVAILABLE: {
-                status = "Available";
-                break;
-            }
-            case telux::common::ServiceStatus::SERVICE_UNAVAILABLE: {
-                status = "Unavailable";
-                break;
-            }
-            case telux::common::ServiceStatus::SERVICE_FAILED: {
-                status = "Failed";
-                break;
-            }
-            default: {
-                status = "Unknown";
-                break;
-            }
+    int init() {
+        telux::common::Status status;
+        telux::common::ServiceStatus serviceStatus;
+        std::promise<telux::common::ServiceStatus> p{};
+
+        /* Step - 1 */
+        auto &platformFactory = telux::platform::PlatformFactory::getInstance();
+
+        /* Step - 2 */
+        fsManager_ = platformFactory.getFsManager(
+                [&p](telux::common::ServiceStatus status) {
+            p.set_value(status);
+        });
+
+        if (!fsManager_) {
+            std::cout << "Can't get IFsManager" << std::endl;
+            return -ENOMEM;
         }
-        std::cout << status << std::endl;
+
+        /* Step - 3 */
+        serviceStatus = p.get_future().get();
+        if (serviceStatus != telux::common::ServiceStatus::SERVICE_AVAILABLE) {
+            std::cout << "File system service unavailable, status " <<
+                static_cast<int>(serviceStatus) << std::endl;
+            return -EIO;
+        }
+
+        /* Step - 4 */
+        status = fsManager_->registerListener(shared_from_this());
+        if (status != telux::common::Status::SUCCESS) {
+            std::cout << "Can't register listener, err " <<
+                static_cast<int>(status) << std::endl;
+            return -EIO;
+        }
+
+        std::cout << "Initialization complete" << std::endl;
+        return 0;
     }
 
-    // [8] Receive EFS restore and backup notifications
-    virtual void OnEfsRestoreEvent(telux::platform::EfsEventInfo event) override {
-        PRINT_NOTIFICATION
-            << ": Received efs event: Restore"
-            << ((event.event == telux::platform::EfsEvent::START) ? " started" : "ended");
-        if (event.event == telux::platform::EfsEvent::END) {
-            std::cout << " with result: " << Utils::getErrorCodeAsString(event.error) << std::endl;
+    int deinit() {
+        telux::common::Status status;
+
+        /* Step - 7 */
+        status = fsManager_->deregisterListener(shared_from_this());
+        if (status != telux::common::Status::SUCCESS) {
+            std::cout << "Can't deregister listener, err " <<
+                static_cast<int>(status) << std::endl;
+            return -EIO;
         }
+
+        return 0;
     }
 
-    virtual void OnEfsBackupEvent(telux::platform::EfsEventInfo event) override {
-        PRINT_NOTIFICATION
-            << ": Received efs event: Backup"
-            << ((event.event == telux::platform::EfsEvent::START) ? " started" : "ended");
-        if (event.event == telux::platform::EfsEvent::END) {
-            std::cout << " with result: " << Utils::getErrorCodeAsString(event.error) << std::endl;
+    int triggerEFSBackup() {
+        telux::common::Status status;
+
+        /* Step - 5 */
+        status = fsManager_->startEfsBackup();
+        if (status != telux::common::Status::SUCCESS) {
+            std::cout << "Can't prepare for OTA, err " <<
+                static_cast<int>(status) << std::endl;
+            return -EIO;
         }
+
+        std::cout << "Backup triggered" << std::endl;
+        return 0;
     }
+
+    /* Step - 6 */
+    void OnEfsBackupEvent(telux::platform::EfsEventInfo eventInfo) override {
+        std::cout << "OnEfsBackupEvent()" << std::endl;
+        std::string result;
+        if (eventInfo.error != telux::common::ErrorCode::SUCCESS) {
+            std::cout << "Failed to backup, err " <<
+                static_cast<int>(eventInfo.error) << std::endl;
+            return;
+        }
+        result = (eventInfo.event == telux::platform::EfsEvent::START) ? "started" : "ended";
+        std::cout << "Backup " << result << std::endl;
+    }
+
+ private:
+    std::shared_ptr<telux::platform::IFsManager> fsManager_;
 };
 
-int main(int argc, char **argv) {
-    std::cout << "********* efs backup restore sample app *********" << std::endl;
+int main(int argc, char *argv[]) {
 
-    // [1] Get platform factory
-    auto &platformFactory = telux::platform::PlatformFactory::getInstance();
+    int ret;
+    std::shared_ptr<EFSBackupRestore> app;
 
-    // [2] Prepare a callback that is invoked when the filesystem sub-system initialization is
-    // complete
-    std::promise<telux::common::ServiceStatus> p;
-    auto initCb = [&p](telux::common::ServiceStatus status) {
-        std::cout << "Received service status: " << static_cast<int>(status) << std::endl;
-        p.set_value(status);
-    };
-
-    // [3] Get the filesystem manager
-    std::shared_ptr<telux::platform::IFsManager> fsManager = platformFactory.getFsManager(initCb);
-    if (fsManager == nullptr) {
-        std::cout << "filesystem manager is nullptr" << std::endl;
-        exit(1);
-    }
-    std::cout << "Obtained filesystem manager" << std::endl;
-
-    // [4] Wait until initialization is complete
-    p.get_future().get();
-    if (fsManager->getServiceStatus() != telux::common::ServiceStatus::SERVICE_AVAILABLE) {
-        std::cout << "Filesystem service not available" << std::endl;
-        exit(1);
-    }
-    std::cout << "Filesystem service is now available" << std::endl;
-
-    // [5] Create the listener object and register as a listener
-    std::shared_ptr<EfsEventListener> efsEventListener = std::make_shared<EfsEventListener>();
-    fsManager->registerListener(efsEventListener);
-
-    // [7] Start EFS backup whenever necessary
-    telux::common::Status status = fsManager->startEfsBackup();
-    if (status != telux::common::Status::SUCCESS) {
-        std::cout << "Unable to start EFS backup: ";
-        Utils::printStatus(status);
-    } else {
-        std::cout << "Request to start EFS backup successful";
+    try {
+        app = std::make_shared<EFSBackupRestore>();
+    } catch (const std::exception& e) {
+        std::cout << "Can't allocate EFSBackupRestore" << std::endl;
+        return -ENOMEM;
     }
 
-    std::cout << "\n\nPress ENTER to exit!!! \n\n";
-    std::cin.ignore();
+    ret = app->init();
+    if (ret < 0) {
+        return ret;
+    }
 
-    // [9] Clean-up
-    fsManager->deregisterListener(efsEventListener);
-    efsEventListener = nullptr;
-    fsManager = nullptr;
+    ret = app->triggerEFSBackup();
+    if (ret < 0) {
+        app->deinit();
+        return ret;
+    }
 
+    /* Wait for receiving all asynchronous responses.
+     * Application specific logic goes here, this wait is just an example */
+    std::this_thread::sleep_for(std::chrono::minutes(3));
+
+    ret = app->deinit();
+    if (ret < 0) {
+        return ret;
+    }
+
+    std::cout << "\nEFS backup app exiting" << std::endl;
     return 0;
 }

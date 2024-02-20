f473f77c0a5f514640405c80913890198332b297
Amisha Singh <quic_amissing@quicinc.com>
2024-02-20 15:22:00 +0530

    audio: fix missed playing last two amr buffers
    After the last two buffers are sent to the DSP,
    DSP can decline to play it. Currently, these two
    buffers are not resent for playback resulting in
    incomplete actual playback. This commit fixes this
    by ensuring that all the buffers are played actually
    before playback thread exits.
    
    Change-Id: I6f96f8bf1cb162acd2854b92e1813546b8b75fb7
    CRs-Fixed: 3665439

--- apps/tests/audio_console_app/PlayMenu.hpp
+++ apps/tests/audio_console_app/PlayMenu.hpp
@@ -102,6 +102,7 @@ private:
     void registerListener();
     void deRegisterListener();
     void closeFile();
+    bool isAMR();
 
     std::shared_ptr<IAudioPlayStream> audioPlayStream_;
     std::shared_ptr<AudioClient> audioClient_;

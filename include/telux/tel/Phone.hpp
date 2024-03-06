cbf1a01e2aa57397f39189b6c0bbf8457323f14c
Ambika Pavan <quic_apavan@quicinc.com>
2024-03-06 10:54:41 +0530

    tel: Getting SignalStrength notification even after setting delta value
    - Add LTE_RSSI for Signal Strength configuration.
    - Update test app.
    
    CRs-Fixed: 3726656
    
    Change-Id: I7fbbc52a27e667c9ba11d662e113ade3f4acdd86

--- include/telux/tel/Phone.hpp
+++ include/telux/tel/Phone.hpp
@@ -340,6 +340,7 @@ public:
     * The threshold range list is as follows. See SignalStrength.hpp for more details.
     * - GSM_RSSI  : -113 to -51 (in dBm)
     * - WCDMA_RSSI: -113 to -51 (in dBm)
+    * - LTE_RSSI  : -113 to -51 (in dBm)
     * - LTE_SNR   : -200 to 300 (in dB)
     * - LTE_RSRQ  : -20 to -3   (in dB)
     * - LTE_RSRP  : -140 to -44 (in dBm)

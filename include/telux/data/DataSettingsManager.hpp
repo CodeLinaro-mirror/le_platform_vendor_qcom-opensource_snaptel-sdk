1a4dc32982a4116aec87d6184640e8b7752e4795
Rishi Gupta <quic_rishgupt@quicinc.com>
2024-03-14 18:33:54 -0700

    treewide: add Inc in the license header
    "Changes from Qualcomm Innovation Center"
    should be "Changes from Qualcomm Innovation
    Center, Inc."
    
    This commit fixes this.
    
    CRs-Fixed: 3760879
    Change-Id: I13e30fea1cec05c68de02a911ba8e9c489b49347

--- include/telux/data/DataSettingsManager.hpp
+++ include/telux/data/DataSettingsManager.hpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2021-2024, Qualcomm Innovation Center, Inc. All rights reserved.
+ *  Copyright (c) 2021-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted (subject to the limitations in the

2f14fa1e6daeb22c1815aa66efd61bea13b5687d
Rishi Gupta <quic_rishgupt@quicinc.com>
2024-03-19 12:17:59 -0700

    audio: add support for multi-format endless playback
    - Add support for the multi-format endless playback.
    
      Client can specify same or different audio format
      files in a single call to the telsdk API.
    
    - Update samples applications as per new API.
    
    - Update audio simulation also to support multi-format
      playback.
    
    CRs-Fixed: 3763614
    Change-Id: Ie31a48fecd232aba851602b683a0f0cb667e36eb

--- apps/samples/audio/repeated_playback_amrwbplus/RepeatedPlayAMRWBPlus.cpp
+++ apps/samples/audio/repeated_playback_amrwbplus/RepeatedPlayAMRWBPlus.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2023-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted (subject to the limitations in the
@@ -33,21 +33,22 @@
  */
 
 /*
- * Steps to play AMR-WB plus audio repeatedly are:
+ * This sample apps demonstrates, how to configure audio streams for each file and
+ * how to define multiple AMR-WB+ files to play repeatedly. The steps are as follows:
  *
  * 1. Get a AudioFactory instance.
- * 2. Get a IAudioPlayer instance from AudioFactory.
- * 3. Implement all listener methods from class IPlayListListener.
+ * 2. Get a IAudioPlayer instance from the AudioFactory.
+ * 3. Implement all listener methods from IPlayListListener class.
  * 4. Define parameters to configure audio stream.
- * 5. Define how given files should be played.
+ * 5. Define how a given file should be played.
  * 6. Start playing the files.
  * 7. When the use case is over, stop the playback.
  *
  * Usage:
  * # repeated_playback_amrwbplus
  *
- * File /data/prompt1.wav is played once and file /data/prompt2.wav is
- * is played played indefinitely on the local speaker.
+ * File /data/prompt1.awbp is played once and file /data/prompt2.awbp is played
+ * indefinitely on the local speaker. Files are mono channel in AMR-WB+ format.
  */
 
 #include <errno.h>
@@ -88,33 +89,42 @@ int RepeatedPlayAMRWBPlus::start(
     bool waitResult = false;
     telux::common::ErrorCode ec;
     telux::audio::StreamConfig sc{};
-    telux::audio::AmrwbpParams amrParams{};
-    telux::audio::PlaybackFile pbFiles1{};
-    telux::audio::PlaybackFile pbFiles2{};
-    std::vector<telux::audio::PlaybackFile> filesToPlay;
+    telux::audio::AmrwbpParams amrParams1{};
+    telux::audio::AmrwbpParams amrParams2{};
+    telux::audio::PlaybackConfig pbCfg1{};
+    telux::audio::PlaybackConfig pbCfg2{};
+    std::vector<telux::audio::PlaybackConfig> pbConfigs;
 
     /* Step - 4 */
-    amrParams.bitWidth = 16;
-    amrParams.frameFormat = telux::audio::AmrwbpFrameFormat::FILE_STORAGE_FORMAT;
+    amrParams1.bitWidth = 16;
+    amrParams1.frameFormat = telux::audio::AmrwbpFrameFormat::FILE_STORAGE_FORMAT;
+    pbCfg1.streamConfig.formatParams = &amrParams1;
+    pbCfg1.streamConfig.type = telux::audio::StreamType::PLAY;
+    pbCfg1.streamConfig.sampleRate = 16000;
+    pbCfg1.streamConfig.format = telux::audio::AudioFormat::AMRWB_PLUS;
+    pbCfg1.streamConfig.channelTypeMask = telux::audio::ChannelType::LEFT;
+    pbCfg1.streamConfig.deviceTypes.emplace_back(telux::audio::DeviceType::DEVICE_TYPE_SPEAKER);
 
-    sc.formatParams = &amrParams;
-    sc.type = telux::audio::StreamType::PLAY;
-    sc.sampleRate = 16000;
-    sc.format = telux::audio::AudioFormat::AMRWB_PLUS;
-    sc.channelTypeMask = telux::audio::ChannelType::LEFT;
-    sc.deviceTypes.emplace_back(telux::audio::DeviceType::DEVICE_TYPE_SPEAKER);
+    amrParams2.bitWidth = 16;
+    amrParams2.frameFormat = telux::audio::AmrwbpFrameFormat::FILE_STORAGE_FORMAT;
+    pbCfg2.streamConfig.formatParams = &amrParams2;
+    pbCfg2.streamConfig.type = telux::audio::StreamType::PLAY;
+    pbCfg2.streamConfig.sampleRate = 16000;
+    pbCfg2.streamConfig.format = telux::audio::AudioFormat::AMRWB_PLUS;
+    pbCfg2.streamConfig.channelTypeMask = telux::audio::ChannelType::LEFT;
+    pbCfg2.streamConfig.deviceTypes.emplace_back(telux::audio::DeviceType::DEVICE_TYPE_SPEAKER);
 
     /* Step - 5 */
     /* Play this file only once */
-    pbFiles1.absoluteFilePath = "/data/prompt1.amr";
-    pbFiles1.repeatInfo.type = telux::audio::RepeatType::COUNT;
-    pbFiles1.repeatInfo.count = 1;
-    filesToPlay.push_back(pbFiles1);
+    pbCfg1.absoluteFilePath = "/data/prompt1.awbp";
+    pbCfg1.repeatInfo.type = telux::audio::RepeatType::COUNT;
+    pbCfg1.repeatInfo.count = 1;
+    pbConfigs.push_back(pbCfg1);
 
     /* Play this file repeatedly */
-    pbFiles2.absoluteFilePath = "/data/prompt2.amr";
-    pbFiles2.repeatInfo.type = telux::audio::RepeatType::INDEFINITELY;
-    filesToPlay.push_back(pbFiles2);
+    pbCfg2.absoluteFilePath = "/data/prompt2.awbp";
+    pbCfg2.repeatInfo.type = telux::audio::RepeatType::INDEFINITELY;
+    pbConfigs.push_back(pbCfg2);
 
    {
     /* First acquire the lock then reset predicates */
@@ -124,7 +134,7 @@ int RepeatedPlayAMRWBPlus::start(
     errorOccurred_ = false;
 
     /* Step - 6 */
-    ec = audioPlayer_->startPlayback(sc, filesToPlay, statusListener);
+    ec = audioPlayer_->startPlayback(pbConfigs, statusListener);
     if (ec != telux::common::ErrorCode::SUCCESS) {
         std::cout << "failed start, err " << static_cast<int>(ec) << std::endl;
         return -EIO;

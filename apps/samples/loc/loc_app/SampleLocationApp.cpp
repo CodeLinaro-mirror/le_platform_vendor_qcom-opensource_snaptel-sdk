f0131b9ac569e7e3899717d54aed066cbd38a7f5
Raxit Raju <quic_raxiraju@quicinc.com>
2024-04-29 16:21:07 +0530

    loc: consolidated sample apps improvements
    CMake changes:
    - Add check for telux-loc package.
    - Use target_include_directories command to
      include required directories.
    - Use target_compile_options command instead
      of directly modifying the compiler options
      directly.
    - Remove generating ARCHIVE target.
    - Bump up version from 2.8.9 to 3.10.2
    
    Code changes:
    - Rework whole app for overall better quality.
    - Show functional flow for use case and telsdk API usage
      distinctly.
    - Explicitly specify what is the purpose of the application.
    - Initialize the local variables as needed.
    - Categories C, CPP and telsdk specific headers inclusion.
    - If error occurs, emit appropriate log, bail out immediately.
    - Remove calling getServiceStatus() as initialization callback
      passed to getFooManager() indicates whether the service is
      available or unavailable.
    - Replace auto keyword with exact class name to make it more
      easy for user to understand the details of the telsdk API.
    - Separate out returning application specific error code and
      telsdk error code.
    - Reduce verbosity as applicable for code and logging.
    - Remove global variables reducing their scope.
    
    CRs-Fixed: 3800686
    
    Change-Id: I48b90ec8dc87cc52c068e69bf55e6afe0fbae5b3

--- apps/samples/loc/loc_app/SampleLocationApp.cpp
+++ apps/samples/loc/loc_app/SampleLocationApp.cpp
@@ -29,7 +29,7 @@
 /*
  *  Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
  *
- *  Copyright (c) 2021 Qualcomm Innovation Center, Inc. All rights reserved.
+ *  Copyright (c) 2021,2024 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted (subject to the limitations in the
@@ -62,78 +62,174 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-
-/**
- * This is a sample program to register and receive location updates
+/*
+ * This application demonstrates how to get location report. The steps are as follows:
+ *
+ * 1. Get a LocationFactory instance.
+ * 2. Get a ILocationManager instance from LocationFactory.
+ * 3. Wait for the location service to become available.
+ * 4. Register the listener which will receive location report.
+ * 5. Start collecting location details.
+ * 6. Execute application specific business logic.
+ * 7. Finally, stop the reports and deregister the listener.
+ *
+ * Usage:
+ * # ./loc_app
  */
+
+#include <errno.h>
 
 #include <iostream>
 #include <memory>
-#include <string>
-#include <vector>
+#include <cstdlib>
+#include <future>
+#include <chrono>
+#include <thread>
 
-#include "telux/loc/LocationDefines.hpp"
+#include <telux/common/CommonDefines.hpp>
 #include "telux/loc/LocationFactory.hpp"
 #include "telux/loc/LocationManager.hpp"
 #include "telux/loc/LocationListener.hpp"
 
-#define PRINT_NOTIFICATION std::cout << "\033[1;35mNOTIFICATION: \033[0m"
+class LocationListener : public telux::loc::ILocationListener,
+                         public std::enable_shared_from_this<LocationListener> {
+ public:
+    int init();
+    int getBasicReports();
+    int deinit();
+    void onBasicLocationUpdate(
+        const std::shared_ptr<telux::loc::ILocationInfoBase> &locationInfo) override;
 
-using namespace telux::loc;
-using namespace telux::common;
-
-class MyLocationListener : public telux::loc::ILocationListener {
-public:
-   void onDetailedLocationUpdate(const std::shared_ptr<telux::loc::ILocationInfoEx> &locationInfo) {
-      std::cout << std::endl;
-      std::cout << "*********************** Detailed Location Report *********************" << std::endl;
-      time_t realtime;
-      realtime = (time_t)(locationInfo->getTimeStamp());
-      PRINT_NOTIFICATION << "Timestamp : " << ctime(&realtime) << std::endl;
-      PRINT_NOTIFICATION << "Latitude : " << locationInfo->getLatitude() << std::endl;
-      PRINT_NOTIFICATION << "Longitude : " << locationInfo->getLongitude() << std::endl;
-      PRINT_NOTIFICATION << "Altitude : " << locationInfo->getAltitude() << std::endl;
-   }
+ private:
+    std::shared_ptr<telux::loc::ILocationManager> locMgr_;
 };
 
-/**
- * Main routine
- */
-int main(int argc, char ** argv) {
-   std::shared_ptr<telux::loc::ILocationListener> myLocationListener
-      = std::make_shared<MyLocationListener>();
-   std::shared_ptr<telux::loc::ILocationManager> locationManager;
-   // Get location manager object
-    std::promise<ServiceStatus> prom = std::promise<ServiceStatus>();
-    auto &locationFactory = LocationFactory::getInstance();
-    locationManager = locationFactory.getLocationManager([&](ServiceStatus status) {
-        prom.set_value(status);
+int LocationListener::init() {
+    telux::common::Status status;
+    telux::common::ServiceStatus serviceStatus;
+    std::promise<telux::common::ServiceStatus> p{};
+
+    /* Step - 1 */
+    auto &locationFactory = telux::loc::LocationFactory::getInstance();
+
+    /* Step - 2 */
+    locMgr_ = locationFactory.getLocationManager(
+            [&p](telux::common::ServiceStatus status) {
+        p.set_value(status);
     });
-    std::chrono::time_point<std::chrono::system_clock> startTime, endTime;
-    startTime = std::chrono::system_clock::now();
-    ServiceStatus locMgrStatus = locationManager->getServiceStatus();
-    if (locMgrStatus != ServiceStatus::SERVICE_AVAILABLE) {
-         std::cout << "Location subsystem is not ready, Please wait" << std::endl;
+
+    if (!locMgr_) {
+        std::cout << "Can't get ILocationManager" << std::endl;
+        return -ENOMEM;
     }
-    locMgrStatus = prom.get_future().get();
-    if (locMgrStatus == ServiceStatus::SERVICE_AVAILABLE) {
-          endTime = std::chrono::system_clock::now();
-          std::chrono::duration<double> elapsedTime = endTime - startTime;
-          std::cout << "Elapsed Time for Subsystems to ready : " << elapsedTime.count()
-              << "s\n" << std::endl;
-    } else {
-          std::cout << "ERROR - Unable to initialize Location subsystem" << std::endl;
-          return -1;
+
+    /* Step - 3 */
+    serviceStatus = p.get_future().get();
+    if (serviceStatus != telux::common::ServiceStatus::SERVICE_AVAILABLE) {
+        std::cout << "Location service unavailable, status " <<
+            static_cast<int>(serviceStatus) << std::endl;
+        return -EIO;
     }
-   // Registering a listener to get location fixes
-   locationManager->registerListenerEx(myLocationListener);
-   // Starting the reports for fixes
-   locationManager->startDetailedReports(1000,NULL);
-   // Exit logic is specific to an application
-   std::cout << "Press enter to exit" << std::endl;
-   std::string input;
-   std::getline(std::cin, input);
-   locationManager->deRegisterListenerEx(myLocationListener);
-   std::cout << "Exiting application..." << std::endl;
-   return 0;
+
+    /* Step - 4 */
+    status = locMgr_->registerListenerEx(shared_from_this());
+    if (status != telux::common::Status::SUCCESS) {
+        std::cout << "Can't register listener, err " <<
+            static_cast<int>(status) << std::endl;
+        return -EIO;
+    }
+
+    std::cout << "Initialization complete" << std::endl;
+    return 0;
+}
+
+int LocationListener::getBasicReports() {
+    telux::common::Status status;
+
+    /* Step - 5 */
+    status = locMgr_->startBasicReports(1000, nullptr);
+    if (status != telux::common::Status::SUCCESS) {
+        std::cout << "Can't start location gathering, err " <<
+            static_cast<int>(status) << std::endl;
+        return -EIO;
+    }
+
+    std::cout << "Request for basic reports placed" << std::endl;
+    return 0;
+}
+
+int LocationListener::deinit() {
+    /* Step - 7 */
+    locMgr_->stopReports(nullptr);
+    telux::common::Status status = locMgr_->deRegisterListenerEx(shared_from_this());
+    if (status != telux::common::Status::SUCCESS) {
+        std::cout << "Can't deregister listener, err " <<
+            static_cast<int>(status) << std::endl;
+        return -EIO;
+    }
+    return 0;
+}
+
+void LocationListener::onBasicLocationUpdate(
+    const std::shared_ptr<telux::loc::ILocationInfoBase> &locationInfo) {
+    std::cout << "***************** Basic Location Report ***************" << std::endl;
+    if(locationInfo->getTimeStamp() != telux::loc::UNKNOWN_TIMESTAMP) {
+        time_t realtime;
+        realtime = (time_t)((locationInfo->getTimeStamp() / 1000));
+        std::cout << "Time stamp: " << locationInfo->getTimeStamp() << " mSec" << std::endl;
+        std::cout << "GMT Time stamp: " << ctime(&realtime);
+    }
+    std::cout << "Latitude: " << std::setprecision(15) << locationInfo->getLatitude() << std::endl
+        << "Longitude: " << std::setprecision(15) << locationInfo->getLongitude() << std::endl
+        << "Altitude: " << std::setprecision(15) << locationInfo->getAltitude() << std::endl
+        << "Speed: " << locationInfo->getSpeed() << std::endl
+        << "Heading: " << locationInfo->getHeading() << std::endl
+        << "Horizontal uncertainty: " << locationInfo->getHorizontalUncertainty() << std::endl
+        << "Vertical uncertainty: " << locationInfo->getVerticalUncertainty() << std::endl
+        << "Speed uncertainty: " << locationInfo->getSpeedUncertainty() << std::endl
+        << "Heading uncertainty: " << locationInfo->getHeadingUncertainty() << std::endl
+        << "Elapsed real time: " << locationInfo->getElapsedRealTime() << std::endl
+        << "Elapsed real time uncertainty: " << locationInfo->getElapsedRealTimeUncertainty()
+        << std::endl
+        << "Time uncertainty: " << locationInfo->getTimeUncMs() << std::endl
+        << "gPTP time: " << locationInfo->getElapsedGptpTime() << std::endl
+        << "gPTP time uncertainty: " << locationInfo->getElapsedGptpTimeUnc() << std::endl;
+    std::cout << "*************************************************************" << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+
+    int ret;
+    std::shared_ptr<LocationListener> app;
+
+    try {
+        app = std::make_shared<LocationListener>();
+    } catch (const std::exception& e) {
+        std::cout << "Can't allocate LocationListener" << std::endl;
+        return -ENOMEM;
+    }
+
+    ret = app->init();
+    if (ret < 0) {
+        return ret;
+    }
+
+    ret = app->getBasicReports();
+    if (ret < 0) {
+        app->deinit();
+        return ret;
+    }
+
+    /* Step - 6 */
+    /* Wait for receiving all asynchronous responses.
+     * Application specific logic goes here, this wait is just an example */
+    std::this_thread::sleep_for(std::chrono::minutes(2));
+
+    ret = app->deinit();
+    if (ret < 0) {
+        return ret;
+    }
+
+    std::cout << "Location app exiting" << std::endl;
+    return 0;
 }

c7dce4f6f0c58e3d476975e25ab45526472bdfd7
Umesh Seth <quic_useth@quicinc.com>
2024-05-08 14:24:00 +0530

    wlan: consolidated sample apps improvements
    CMake changes:
    - Add check for telux-wlan package.
    - Add missing CMakeLists.txt.
    - Use target_include_directories command to
      include required directories.
    - Use target_compile_options command instead
      of directly modifying the compiler options
      directly.
    - Remove generating ARCHIVE target.
    - Bump up version from 2.8.9 to 3.10.2
    - Correctly set directory structure in CMake file.
    
    Code changes:
    - Rework whole app for overall better quality.
    - Show functional flow for use case and telsdk API usage
      distinctly.
    - Explicitly specify what is the purpose of the application.
    - Initialize the local variables as needed.
    - Categories C, CPP and telsdk specific headers inclusion.
    - If error occurs, emit appropriate log, bail out immediately.
    - Remove calling getServiceStatus() as initialization callback
      passed to getFooManager() indicates whether the service is
      available or unavailable.
    - Replace auto keyword with exact class name to make it more
      easy for user to understand the details of the telsdk API.
    - Separate out returning application specific error code and
      telsdk error code.
    - Reduce verbosity as applicable for code and logging.
    - Remove global variables reducing their scope.
    - Rename readme file to Readme.
    
    CRs-Fixed: 3805024
    
    Change-Id: I2067b9a79145008930ce5cddf90f46c90fe4f5b7

--- apps/samples/wlan/wlan_config_app/WlanConfigApp.cpp
+++ apps/samples/wlan/wlan_config_app/WlanConfigApp.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022,2024 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted (subject to the limitations in the
@@ -32,138 +32,226 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/*
+ * This application demonstrates how to configure the WLAN. The steps are as follows:
+ *
+ * 1. Get a WlanFactory instance.
+ * 2. Get a IWlanDeviceManager instance from the WlanFactory.
+ * 3. Wait for the WLAN service to become available.
+ * 4. Register a listener that will receive WLAN state change updates.
+ * 5. Disable WLAN, if it is enabled currently.
+ * 6. Update WLAN configuration by specifying number of the
+ *    access points and number of the stations.
+ * 7. Enable the WLAN for the configuration to take effect.
+ * 8. Deregister the listener.
+ *
+ * Usage:
+ * # ./wlan_config_app <number of APs> <number of STAs>
+ *
+ * Example - ./wlan_config_app 1 1
+ *
+ * File hostapd.conf and wpa_supplicant.conf contains settings for
+ * the AP and STA respectively. Please refer Readme file for the
+ * details about these files.
+ */
+
+#include <errno.h>
+
 #include <iostream>
 #include <memory>
 #include <cstdlib>
 #include <future>
 
-#include <telux/wlan/WlanDefines.hpp>
+#include <telux/common/CommonDefines.hpp>
 #include <telux/wlan/WlanFactory.hpp>
 #include <telux/wlan/WlanDeviceManager.hpp>
 
+class WLANConfigurator : public telux::wlan::IWlanListener,
+                         public std::enable_shared_from_this<WLANConfigurator> {
+ public:
+    int init() {
+        telux::common::ErrorCode ec;
+        telux::common::ServiceStatus serviceStatus;
+        std::promise<telux::common::ServiceStatus> p{};
 
-/**
- * @file: WlanConfigApp.cpp
- *
- * @brief: Simple application to Configure and enable Wlan
- *         ./wlan_config_app <Num of APs> <Num of Sta>
- *         Settings of AP and Sta will be encapsulated in
- *         hostapd.conf and wpa_supplicant.conf correspondingly.
- *         See readme file for location of those files.
- */
+        /* Step - 1 */
+        auto &wlanFactory = telux::wlan::WlanFactory::getInstance();
 
-class NotificationListener: public telux::wlan::IWlanListener {
-public:
-   void onEnableChanged(bool enable) {
-      promise_.set_value(enable);
-   }
+        /* Step - 2 */
+        wlanDevMgr_ = wlanFactory.getWlanDeviceManager(
+                [&p](telux::common::ServiceStatus status) {
+            p.set_value(status);
+        });
 
-   bool getEnableStatus() {
-      return promise_.get_future().get();
-   }
+        if (!wlanDevMgr_) {
+            std::cout << "Can't get IWlanDeviceManager" << std::endl;
+            return -ENOMEM;
+        }
 
-   void resetPromise() {
-      promise_ = std::promise<bool>();
-   }
-private:
-   std::promise<bool> promise_;
+        /* Step - 3 */
+        serviceStatus = p.get_future().get();
+        if (serviceStatus != telux::common::ServiceStatus::SERVICE_AVAILABLE) {
+            std::cout << "WLAN service unavailable, status " <<
+                static_cast<int>(serviceStatus) << std::endl;
+            return -EIO;
+        }
+
+        /* Step - 4 */
+        ec = wlanDevMgr_->registerListener(shared_from_this());
+        if (ec != telux::common::ErrorCode::SUCCESS) {
+            std::cout << "Can't register listener" << std::endl;
+            return -EIO;
+        }
+
+        std::cout << "Initialization complete" << std::endl;
+        return 0;
+    }
+
+    int deinit() {
+        telux::common::ErrorCode ec;
+
+        /* Step - 8 */
+        ec = wlanDevMgr_->deregisterListener(shared_from_this());
+        if (ec != telux::common::ErrorCode::SUCCESS) {
+            std::cout << "Can't deregister listener, err " <<
+                static_cast<int>(ec) << std::endl;
+            return -EIO;
+        }
+
+        return 0;
+    }
+
+    int disableWLAN() {
+        bool isEnabled = false;
+        telux::common::ErrorCode ec;
+        std::vector<telux::wlan::InterfaceStatus> ifaceStatus;
+
+        ec = wlanDevMgr_->getStatus(isEnabled, ifaceStatus);
+        if (ec != telux::common::ErrorCode::SUCCESS) {
+            std::cout << "Can't get current state, err " <<
+                static_cast<int>(ec) << std::endl;
+            return -EIO;
+        }
+
+        if (isEnabled) {
+            promise_ = std::promise<bool>();
+
+            /* Step - 5 */
+            ec = wlanDevMgr_->enable(false);
+            if (ec != telux::common::ErrorCode::SUCCESS) {
+                std::cout << "Can't disable WLAN, err " <<
+                    static_cast<int>(ec) << std::endl;
+                return -EIO;
+            }
+
+            isEnabled = promise_.get_future().get();
+            if (isEnabled) {
+                std::cout << "Failed to disable WLAN" << std::endl;
+                return -EIO;
+            }
+        }
+
+        std::cout << "WLAN disabled" << std::endl;
+        return 0;
+    }
+
+    int updateConfiguration(int APCount, int STACount) {
+        telux::common::ErrorCode ec;
+
+        /* Step - 6 */
+        ec = wlanDevMgr_->setMode(APCount, STACount);
+        if (ec != telux::common::ErrorCode::SUCCESS) {
+            std::cout << "Can't set config, err " << static_cast<int>(ec) << std::endl;
+            return -EIO;
+        }
+
+        std::cout << "\nMode set successfully" << std::endl;
+        return 0;
+    }
+
+    int enableWLAN() {
+        bool isEnabled = false;
+        telux::common::ErrorCode ec;
+
+        promise_ = std::promise<bool>();
+
+        /* Step - 7 */
+        ec = wlanDevMgr_->enable(true);
+        if (ec != telux::common::ErrorCode::SUCCESS) {
+            std::cout << "Can't enable WLAN, err " << static_cast<int>(ec) << std::endl;
+            return -EIO;
+        }
+
+        isEnabled = promise_.get_future().get();
+        if (!isEnabled) {
+            std::cout << "Failed to enable WLAN" << std::endl;
+            return -EIO;
+        }
+
+        std::cout << "WLAN enabled" << std::endl;
+        return 0;
+    }
+
+    void onEnableChanged(bool enable) {
+        std::cout << "\nonEnableChanged()" << std::endl;
+        std::cout << "New value: " << enable << std::endl;
+        promise_.set_value(enable);
+    }
+
+ private:
+    std::promise<bool> promise_;
+    std::shared_ptr<telux::wlan::IWlanDeviceManager> wlanDevMgr_;
 };
 
 int main(int argc, char *argv[]) {
-   std::promise<telux::common::ServiceStatus> initPromise;
-   telux::common::ErrorCode errCode = telux::common::ErrorCode::SUCCESS;
-   std::shared_ptr<telux::wlan::IWlanDeviceManager> wlanDevMgr = nullptr;
-   auto listener = std::make_shared<NotificationListener>();
-   telux::common::ServiceStatus subSystemStatus = telux::common::ServiceStatus::SERVICE_FAILED;
 
-   if(argc == 3) {
-      int numAp = std::atoi(argv[1]);
-      int numSta = std::atoi(argv[2]);
+    int ret, numberOfAP, numberOfSTA;
+    std::shared_ptr<WLANConfigurator> app;
 
-      // [1] Instantiate initialization callback - this is optional
-      auto initCb = [&](telux::common::ServiceStatus status) {
-         initPromise.set_value(status);
-      };
+    if (argc != 3) {
+        std::cout <<
+            "Usage: ./wlan_config_app <number of APs> <number of STAs>" << std::endl;
+        return -EINVAL;
+    }
 
-      // [2] Get the WlanFactory and Device Manager instance
-      auto &wlanFactory = telux::wlan::WlanFactory::getInstance();
-      do {
-         wlanDevMgr  = wlanFactory.getWlanDeviceManager(initCb);
-         if (wlanDevMgr) {
-            // [3] Check if Device manager is ready
-            std::cout <<
-                  "\nInitializing Wlan subsystem Please wait ..." << std::endl;
-            subSystemStatus = initPromise.get_future().get();
-         }
-         if (subSystemStatus == telux::common::ServiceStatus::SERVICE_AVAILABLE) {
-            std::cout << " *** Wlan SubSystem is Ready *** " << std::endl;
-         }
-         else {
-            std::cout << " *** Unable to initialize Wlan subsystem *** " << std::endl;
-            break;
-         }
+    numberOfAP = std::atoi(argv[1]);
+    numberOfSTA = std::atoi(argv[2]);
 
-         // [4] If Wlan is enabled, disable Wlan - this necessary before changing configuration
-         bool enableStat = false;
-         std::cout << "Checking Wlan Status... Please wait" << std::endl;
-         std::vector<telux::wlan::InterfaceStatus> ifStatus;
-         if(telux::common::ErrorCode::SUCCESS != wlanDevMgr->getStatus(enableStat, ifStatus)) {
-            std::cout << "Failed to retrieve Wlan status... quitting application" <<std::endl;
-            break;
-         }
-         std::cout << "Wlan is " << ((enableStat)? "Enabled":"Disabled") << std::endl;
-         if(enableStat) {
-            std::cout << "Disabling Wlan before changing configuration... Please wait" << std::endl;
-            wlanDevMgr->registerListener(listener);
-            //Ensure callback returns SUCCESS and indication for Wlan disablement is received
-            if((telux::common::ErrorCode::SUCCESS != wlanDevMgr->enable(false)) ||
-               (true == listener->getEnableStatus())) {
-               std::cout << "Failed to disable Wlan ... quitting application" <<std::endl;
-               break;
-            }
-         }
-         // [5] Set Wlan configuration
-         std::cout << "Setting Wlan Config .... Please wait" << std::endl;
-         errCode = wlanDevMgr->setMode(numAp, numSta);
-         if(telux::common::ErrorCode::SUCCESS != errCode) {
-            std::cout << "Failed to set Wlan configuration... Error Code: "
-                      << static_cast<int>(errCode) << ". quitting application" <<std::endl;
-            break;
-         }
+    try {
+        app = std::make_shared<WLANConfigurator>();
+    } catch (const std::exception& e) {
+        std::cout << "Can't allocate WLANConfigurator" << std::endl;
+        return -ENOMEM;
+    }
 
-         // [6] Enable Wlan for new configuration to take effect
-         listener->resetPromise();
-         std::cout << "Enabling Wlan to activate new configuration... Please wait" << std::endl;
-         //Ensure callback returns SUCCESS and indication for Wlan enablement is received
-         if((telux::common::ErrorCode::SUCCESS != wlanDevMgr->enable(true)) ||
-            (false == listener->getEnableStatus())) {
-            std::cout << "Failed to enable Wlan ... quitting application" <<std::endl;
-            break;
-         }
-      } while(0);
+    ret = app->init();
+    if (ret < 0) {
+        return ret;
+    }
 
-      // [7] Cleanup
-      if(wlanDevMgr) {
-         wlanDevMgr->deregisterListener(listener);
-         wlanDevMgr = nullptr;
-      }
-      if(listener) {
-         listener = nullptr;
-      }
-   } else {
-      std::cout << "\n Invalid argument!!! \n\n";
-      std::cout << "\n Sample command is: \n";
-      std::cout << "\n\t ./wlan_config_app <Num of APs> <Num of Sta>";
-      std::cout << std::endl;
-      std::cout << "\n\t\t Num of APs     Number of Access Points to be configured";
-      std::cout << "\n\t\t Num of Sta     Number of Stations to be configured";
-      std::cout << std::endl;
-      std::cout << "\n\t ./wlan_config_app 1 1 --> to configure and enable AP+STA\n";
-   }
+    ret = app->disableWLAN();
+    if (ret < 0) {
+        app->deinit();
+        return ret;
+    }
 
-   // [7] Cleaning up and exit the application
-   std::cout << "\n\nPress ENTER to exit!!! \n\n";
-   std::cin.ignore();
+    ret = app->updateConfiguration(numberOfAP, numberOfSTA);
+    if (ret < 0) {
+        app->deinit();
+        return ret;
+    }
 
-   return 0;
+    ret = app->enableWLAN();
+    if (ret < 0) {
+        app->deinit();
+        return ret;
+    }
+
+    ret = app->deinit();
+    if (ret < 0) {
+        return ret;
+    }
+
+    std::cout << "\nWlan app exiting" << std::endl;
+    return 0;
 }

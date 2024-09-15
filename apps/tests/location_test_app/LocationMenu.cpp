6ccfc99520dec4c3dc2461dfa592e33622e2ef74
Shaishav Shroff <quic_shaishro@quicinc.com>
2024-09-15 06:28:51 +0530

    telux: add leprop as supplementary group for location_test_app
    add leprop as supplementary group in location_test_App for dac_override denial fixes
    
    CRs-Fixed:3895356
    Change-Id: I5a7cf55ff30f1b0d09e22def03b8eebffb5c5f08

--- apps/tests/location_test_app/LocationMenu.cpp
+++ apps/tests/location_test_app/LocationMenu.cpp
@@ -2184,7 +2184,8 @@ int main(int argc, char **argv) {
         "Release name: " + sdkReleaseName;
     LocationMenu locationMenu(appName, "location> ");
     // Setting required secondary groups for SDK file/diag logging
-    std::vector<std::string> supplementaryGrps{"system", "diag", "locclient", "logd", "dlt"};
+    std::vector<std::string> supplementaryGrps{"system", "diag", "locclient", "logd",
+        "dlt", "leprop"};
     int rc = Utils::setSupplementaryGroups(supplementaryGrps);
     if (rc == -1){
         std::cout << "Adding supplementary groups failed!" << std::endl;

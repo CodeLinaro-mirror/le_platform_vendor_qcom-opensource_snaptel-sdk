bde85a9a6639db8315406c9f723dde3a858ff100
Rahul Pasapula <quic_rpasapul@quicinc.com>
2025-01-07 20:32:40 +0530

    tel: Add fix to reduce growing memory in case of SSR
    Added virtual destructor to avoid memory leak.
    
    CRs-Fixed: 3823494
    
    Change-Id: I822d240c1b901907bb15cef7e291ee80b199c5d7
    (cherry picked from commit 17d8668f7e6549402ef11b6f50b30c45570dc237)

--- include/telux/tel/CardFileHandler.hpp
+++ include/telux/tel/CardFileHandler.hpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ *  Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted (subject to the limitations in the
@@ -262,6 +262,9 @@ class ICardFileHandler {
      *
      */
     virtual SlotId getSlotId() = 0;
+
+    virtual ~ICardFileHandler() {
+    }
 
 };
 

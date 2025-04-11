34aaa7027a48ec1b337e4261dd1fed94c4633b1a
Vishal Sadashiv Shetty <quic_vishalsa@quicinc.com>
2025-04-11 19:17:48 +0530

    data: introducing new extended error code.
    - adding extended error code for PROFILE_ALREADY_PRESENT scenario.
    
    CRs-Fixed: 4088817
    Change-Id: Ia01ef6b21748af5a8a3c2a122f5659d45bb180a6

--- apps/common/utils/Utils.cpp
+++ apps/common/utils/Utils.cpp
@@ -326,6 +326,8 @@ std::map<telux::common::ErrorCode, std::string> errorCodeToStringMap_ = {
    {telux::common::ErrorCode::REG_RESULT_ERR_OUT_OF_PROFILES, "REG_RESULT_ERR_OUT_OF_PROFILES"},
    {telux::common::ErrorCode::REG_RESULT_NO_EMERGENCY_PDN_SUPPORT,
     "REG_RESULT_NO_EMERGENCY_PDN_SUPPORT"},
+   {telux::common::ErrorCode::REG_RESULT_PROFILE_ALREADY_PRESENT,
+      "REG_RESULT_PROFILE_ALREADY_PRESENT"},
    {telux::common::ErrorCode::DS_PROFILE_3GPP_INVAL_PROFILE_FAMILY,
     "DS_PROFILE_3GPP_INVAL_PROFILE_FAMILY"},
    {telux::common::ErrorCode::DS_PROFILE_3GPP_ACCESS_ERR, "DS_PROFILE_3GPP_ACCESS_ERR"},

c23dea22c9ccf5d8c4dec0d183711473ed2ab6ff
jie Chen <jie@qti.qualcomm.com>
2025-06-09 05:58:28 -0400

    telux: Support network time in chrony-sock
    1. Enable network time and power notification as configured;
    2. Adjust system time on receiving the first GNSS/CV2X time or
       each network time update;
    3. Support updating delta time to be used for early-settime
       whenever adjusting system time in chrony-sock.
    4. Refactored of the code. Create a class to encapsulate all
       functionalities. Removed option '-a' for enabling CV2X time
       and '-o' for setting threshold. Added "enable.cv2x.time" and
       "time.hysteresis.threshold" in telux config file for those purposes.
    
    CRs-Fixed: 4176089
    
    Change-Id: Ia3be8ba33d37291cb19fde87d10a0829529fc905
    Signed-off-by: jie Chen <jie@qti.qualcomm.com>

---
+++ apps/reference/chrony-sock/MyUtils.hpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
+/**
+ * @file: MyUtils.hpp
+ *
+ * @brief: Provides utility functions.
+ */
+
+#ifndef MY_UTILS_HPP__
+#define MY_UTILS_HPP__
+
+#include <memory>
+#include <deque>
+#include <future>
+#include <mutex>
+#include <syslog.h>
+#include <stdarg.h>
+
+extern bool enableDebug;
+extern bool enableSyslog;
+
+void chronylog(int level, const char *fmt, ...);
+
+#define LOGI(fmt, args...) \
+    chronylog(LOG_NOTICE, "[I][%s:%d] " fmt, __func__, __LINE__, ## args)
+#define LOGD(fmt, args...) \
+    chronylog(LOG_DEBUG, "[D][%s:%d] " fmt, __func__, __LINE__, ## args)
+#define LOGE(fmt, args...) \
+    chronylog(LOG_ERR, "[E][%s:%d] " fmt, __func__, __LINE__, ## args)
+
+class AsyncThread {
+public:
+
+    /*Join existing threads from myself, it should NOT be called from 1 of my own thread context*/
+    void joinAllThreads();
+
+    /*Add a new future into the thread queue, it will be executed sometime later, currently only
+     *support furture constructed by std::launch::async, not support std::launch::deferred*/
+    void addToQueue(std::shared_future<void> &f);
+private:
+
+    /*Remove completed futures from the thread queue*/
+    void purgeCompletedFutures();
+
+     std::deque<std::shared_future<void>> threadQueue_;
+     /*Lock to protect when access the above threadQueue_*/
+     std::mutex mutex_;
+     bool exit_ = false;
+};
+
+#endif

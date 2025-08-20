2fbbc0cf37b0e7d8b1261239fbf6f04b9fe1d535
Shilpi Shah <shilshah@qti.qualcomm.com>
2025-08-20 08:22:06 +0530

    tel: Update documentation for redial time gap
    - Added documentation for call origination and
    call drop failure when NG eCall is enabled.
    
    CRs-Fixed: 4276209
    
    Change-Id: I1ebbd46b1d22748995cc5aae43a4607a8a398db8

--- include/telux/tel/ECallDefines.hpp
+++ include/telux/tel/ECallDefines.hpp
@@ -106,7 +106,7 @@ enum class ReasonType {
     NONE = 0,                   /**< Redial reason is NONE */
     CALL_ORIG_FAILURE = 1,      /**< Redial will be attempted due to eCall origination failure */
     CALL_DROP = 2,              /**< Redial will be attempted as the eCall is terminated before the
-                                     reciept of MSD Transmission status */
+                                     receipt of MSD Transmission status */
     MAX_REDIAL_ATTEMPTED = 3,   /**< Redial will not be attempted as the maximum redial count
                                      is reached */
     CALL_CONNECTED = 4,         /**< Redial will not be attempted as the eCall is connected
@@ -128,9 +128,13 @@ struct ECallRedialInfo {
  */
 
 enum class RedialConfigType {
-    CALL_DROP = 0,  /**< Redial configuration for eCall termination before reciept of MSD
+    CALL_DROP = 0,  /**< Redial configuration for eCall termination before receipt of MSD
                          Transmission status */
-    CALL_ORIG = 1,  /**< Redial configuration for eCall origination failure */
+    CALL_ORIG = 1,  /**< Redial configuration for eCall origination is applicable only to CS eCall.
+                         It must not be applied on platforms that support both NG eCall and CS
+                         eCall, as redial behavior in such cases is governed by the modem’s domain
+                         selection process and redial attempts are handled internally by the
+                         modem. */
 };
 
 /**

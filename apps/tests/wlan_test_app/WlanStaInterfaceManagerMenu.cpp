c4f40291452b81f803a65d8ad430f5ae3416b7f2
Vishal Sadashiv Shetty <vishalsa@qti.qualcomm.com>
2025-10-16 14:49:56 +0530

    wlan: support input of SSID and passphrase with embedded spaces.
    CRs-Fixed: 4323159
    Change-Id: I952c9f8de658d40e4ca2678bfbbd6dec37b55e91

--- apps/tests/wlan_test_app/WlanStaInterfaceManagerMenu.cpp
+++ apps/tests/wlan_test_app/WlanStaInterfaceManagerMenu.cpp
@@ -252,9 +252,9 @@ void WlanStaInterfaceManagerMenu::addNetworkConfig(std::vector<std::string> user
 
     telux::wlan::StaNetworkConfigEntry staNetConfigEntry = {};
     std::string ssid = "";
+    char delimiter = '\n';
     std::cout << "Enter SSID (Without Quotes): ";
-    std::cin >> ssid;
-    Utils::validateInput(ssid);
+    std::getline(std::cin, ssid, delimiter);
     staNetConfigEntry.ssid = ssid;
 
     int input = 0;
@@ -265,11 +265,9 @@ void WlanStaInterfaceManagerMenu::addNetworkConfig(std::vector<std::string> user
     if(input) {
         std::string passPhrase = "";
         std::cout << "Enter passphrase (Without Quotes): ";
-        std::cin >> passPhrase;
-        Utils::validateInput(passPhrase);
+        std::getline(std::cin, passPhrase, delimiter);
         staNetConfigEntry.passPhrase = passPhrase;
     }
-
 
     int userResp = 0;
     std::cout << "Enter STA operational band type \
@@ -286,7 +284,6 @@ void WlanStaInterfaceManagerMenu::addNetworkConfig(std::vector<std::string> user
         staNetConfigEntry.band = telux::wlan::BandType::BAND_6GHZ;
     }
 
-    char delimiter = '\n';
     std::string priority;
     std::cout << "Enter priority (optional) : ";
     std::getline(std::cin, priority, delimiter);

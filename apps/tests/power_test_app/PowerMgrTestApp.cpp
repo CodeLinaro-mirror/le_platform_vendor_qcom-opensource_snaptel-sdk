fdc843c9e9723f0b42b6097ef7de2f53a16e6192
bnandago <bnandago@qti.qualcomm.com>
2025-11-21 17:14:20 +0530

    power:Add missing -w parameter to telux_power_test_app help menu.
    Fix: Included the -w option and its description in the help output for telux_power_test_app to ensure all supported parameters are documented.
    
    CRs-Fixed: 4343922
    Change-Id: I9d72b60dc7741e9fead921c8165f6976f1a28ec5

--- apps/tests/power_test_app/PowerMgrTestApp.cpp
+++ apps/tests/power_test_app/PowerMgrTestApp.cpp
@@ -79,7 +79,7 @@ static void printTcuActivityState(TcuActivityState state, std::string machineNam
 
 static void printHelp() {
     std::cout << "-----------------------------------------------" << std::endl;
-    std::cout << "./telux_power_test_app <-l> <-s> <-r> <-p> <-c> <-t> <-T> <-h>" << std::endl;
+    std::cout << "./telux_power_test_app <-l> <-s> <-r> <-p> <-c> <-t> <-T> <-w> <-h>" << std::endl;
     std::cout << "Operations: " << std::endl;
     std::cout << "   -l : listen to TCU-activity state updates (as SLAVE)" << std::endl;
     std::cout << "   -s : send SUSPEND command (as MASTER)" << std::endl;
@@ -110,6 +110,7 @@ static void printHelp() {
               << "        e.g. telux_power_test_app -T qcom,mdm" << std::endl
               << "             telux_power_test_app -T qcom,televm" << std::endl;
     std::cout << "   -c : open interactive console (as MASTER)" << std::endl;
+    std::cout << "   -w : register for wakeup indications" << std::endl;
     std::cout << "   -h : print the help menu" << std::endl;
 }
 

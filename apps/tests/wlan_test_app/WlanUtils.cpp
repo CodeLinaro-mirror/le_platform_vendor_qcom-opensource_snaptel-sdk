631fe78bac241355efcde9c46280c71175dffad0
bnandago <bnandago@qti.qualcomm.com>
2025-11-21 16:51:20 +0530

    Wlan: Reorder Wi-Fi scan result table columns for better readability
    Fix: Updated the output format in printScanResult() to display columns    in the following order:
    Frequency | Signal Level | SSID | Flags
    
    CRs-Fixed: 4346403
    Change-Id: I072cec712bfad61f887a7cf9f926ef76f0f2aaf5

--- apps/tests/wlan_test_app/WlanUtils.cpp
+++ apps/tests/wlan_test_app/WlanUtils.cpp
@@ -439,15 +439,16 @@ void WlanUtils::printScanResult(const telux::wlan::StaScanResult &staScanResult)
        std::cout << std::left << std::setw(18) << "\nBSSID "
        << std::setw(10) << " | Frequency "
        << std::setw(10) << " | Signal Level "
-       << std::setw(23) << " | Flags "
-       << " | SSID\n" << std::endl;
+       << std::setw(35) << " | SSID "
+       << " | Flags\n" << std::endl;
 
       for(auto& externalAp:staScanResult.externalApList) {
          std::cout << std::left << std::setw(20) << externalAp.bssid
          << std::setw(10) << RadioTypeToString(externalAp.band)
          << std::setw(10) << externalAp.signalStrength
-         << std::setw(30) << externalAp.securityFlags
-         << externalAp.ssid << std::endl;
+         << std::setw(35) << externalAp.ssid
+         << externalAp.securityFlags << std::endl;
+
      }
        std::cout << std::endl;
    } else {

64140a481fe3b9c426c9bd436a73cc7d32f35bf2
Rahul Pasapula <rpasapul@qti.qualcomm.com>
2025-12-02 19:00:05 +0530

    telux: Adding support for Band info change notifications.
    - Added handling for RF band info change notifications.
    
    CRs-Fixed: 3113916
    Change-Id: I09da184d136ff3ed1bc94199c8e4bf9b2794ad3a

--- include/telux/tel/ServingSystemManager.hpp
+++ include/telux/tel/ServingSystemManager.hpp
@@ -27,40 +27,9 @@
  *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/*
- *  Changes from Qualcomm Innovation Center are provided under the following license:
- *
- *  Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted (subject to the limitations in the
- *  disclaimer below) provided that the following conditions are met:
- *
- *      * Redistributions of source code must retain the above copyright
- *        notice, this list of conditions and the following disclaimer.
- *
- *      * Redistributions in binary form must reproduce the above
- *        copyright notice, this list of conditions and the following
- *        disclaimer in the documentation and/or other materials provided
- *        with the distribution.
- *
- *      * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
- *        contributors may be used to endorse or promote products derived
- *        from this software without specific prior written permission.
- *
- *  NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
- *  GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
- *  HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
- *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- *  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
- *  IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- *  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
- *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/* Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
 /**
@@ -136,6 +105,212 @@ enum class DcnrRestriction {
 struct DcStatus {
    EndcAvailability endcAvailability;     /**< ENDC availability */
    DcnrRestriction  dcnrRestriction;      /**< DCNR restriction */
+};
+
+/**
+ * Defines RF Bands.
+ */
+enum class RFBand {
+   INVALID = -1,
+   BC_0 = 0,
+   BC_1 = 1,
+   BC_3 = 3,
+   BC_4 = 4,
+   BC_5 = 5,
+   BC_6 = 6,
+   BC_7 = 7,
+   BC_8 = 8,
+   BC_9 = 9,
+   BC_10 = 10,
+   BC_11 = 11,
+   BC_12 = 12,
+   BC_13 = 13,
+   BC_14 = 14,
+   BC_15 = 15,
+   BC_16 = 16,
+   BC_17 = 17,
+   BC_18 = 18,
+   BC_19 = 19,
+   GSM_450 = 40,
+   GSM_480 = 41,
+   GSM_750 = 42,
+   GSM_850 = 43,
+   GSM_900_EXTENDED = 44,
+   GSM_900_PRIMARY = 45,
+   GSM_900_RAILWAYS = 46,
+   GSM_1800 = 47,
+   GSM_1900 = 48,
+   WCDMA_2100 = 80,
+   WCDMA_PCS_1900 = 81,
+   WCDMA_DCS_1800 = 82,
+   WCDMA_1700_US = 83,
+   WCDMA_850 = 84,
+   WCDMA_800 = 85,
+   WCDMA_2600 = 86,
+   WCDMA_900 = 87,
+   WCDMA_1700_JAPAN = 88,
+   WCDMA_1500_JAPAN = 90,
+   WCDMA_850_JAPAN = 91,
+   E_UTRA_OPERATING_BAND_1 = 120,
+   E_UTRA_OPERATING_BAND_2 = 121,
+   E_UTRA_OPERATING_BAND_3 = 122,
+   E_UTRA_OPERATING_BAND_4 = 123,
+   E_UTRA_OPERATING_BAND_5 = 124,
+   E_UTRA_OPERATING_BAND_6 = 125,
+   E_UTRA_OPERATING_BAND_7 = 126,
+   E_UTRA_OPERATING_BAND_8 = 127,
+   E_UTRA_OPERATING_BAND_9 = 128,
+   E_UTRA_OPERATING_BAND_10 = 129,
+   E_UTRA_OPERATING_BAND_11 = 130,
+   E_UTRA_OPERATING_BAND_12 = 131,
+   E_UTRA_OPERATING_BAND_13 = 132,
+   E_UTRA_OPERATING_BAND_14 = 133,
+   E_UTRA_OPERATING_BAND_17 = 134,
+   E_UTRA_OPERATING_BAND_33 = 135,
+   E_UTRA_OPERATING_BAND_34 = 136,
+   E_UTRA_OPERATING_BAND_35 = 137,
+   E_UTRA_OPERATING_BAND_36 = 138,
+   E_UTRA_OPERATING_BAND_37 = 139,
+   E_UTRA_OPERATING_BAND_38 = 140,
+   E_UTRA_OPERATING_BAND_39 = 141,
+   E_UTRA_OPERATING_BAND_40 = 142,
+   E_UTRA_OPERATING_BAND_18 = 143,
+   E_UTRA_OPERATING_BAND_19 = 144,
+   E_UTRA_OPERATING_BAND_20 = 145,
+   E_UTRA_OPERATING_BAND_21 = 146,
+   E_UTRA_OPERATING_BAND_24 = 147,
+   E_UTRA_OPERATING_BAND_25 = 148,
+   E_UTRA_OPERATING_BAND_41 = 149,
+   E_UTRA_OPERATING_BAND_42 = 150,
+   E_UTRA_OPERATING_BAND_43 = 151,
+   E_UTRA_OPERATING_BAND_23 = 152,
+   E_UTRA_OPERATING_BAND_26 = 153,
+   E_UTRA_OPERATING_BAND_32 = 154,
+   E_UTRA_OPERATING_BAND_125 = 155,
+   E_UTRA_OPERATING_BAND_126 = 156,
+   E_UTRA_OPERATING_BAND_127 = 157,
+   E_UTRA_OPERATING_BAND_28 = 158,
+   E_UTRA_OPERATING_BAND_29 = 159,
+   E_UTRA_OPERATING_BAND_30 = 160,
+   E_UTRA_OPERATING_BAND_66 = 161,
+   E_UTRA_OPERATING_BAND_250 = 162,
+   E_UTRA_OPERATING_BAND_46 = 163,
+   E_UTRA_OPERATING_BAND_27 = 164,
+   E_UTRA_OPERATING_BAND_31 = 165,
+   E_UTRA_OPERATING_BAND_71 = 166,
+   E_UTRA_OPERATING_BAND_47 = 167,
+   E_UTRA_OPERATING_BAND_48 = 168,
+   E_UTRA_OPERATING_BAND_67 = 169,
+   E_UTRA_OPERATING_BAND_68 = 170,
+   E_UTRA_OPERATING_BAND_49 = 171,
+   E_UTRA_OPERATING_BAND_85 = 172,
+   E_UTRA_OPERATING_BAND_72 = 173,
+   E_UTRA_OPERATING_BAND_73 = 174,
+   E_UTRA_OPERATING_BAND_86 = 175,
+   E_UTRA_OPERATING_BAND_53 = 176,
+   E_UTRA_OPERATING_BAND_87 = 177,
+   E_UTRA_OPERATING_BAND_88 = 178,
+   E_UTRA_OPERATING_BAND_70 = 179,
+   TDSCDMA_BAND_A = 200,
+   TDSCDMA_BAND_B = 201,
+   TDSCDMA_BAND_C = 202,
+   TDSCDMA_BAND_D = 203,
+   TDSCDMA_BAND_E = 204,
+   TDSCDMA_BAND_F = 205,
+   NR5G_BAND_1 = 250,
+   NR5G_BAND_2 = 251,
+   NR5G_BAND_3 = 252,
+   NR5G_BAND_5 = 253,
+   NR5G_BAND_7 = 254,
+   NR5G_BAND_8 = 255,
+   NR5G_BAND_20 = 256,
+   NR5G_BAND_28 = 257,
+   NR5G_BAND_38 = 258,
+   NR5G_BAND_41 = 259,
+   NR5G_BAND_50 = 260,
+   NR5G_BAND_51 = 261,
+   NR5G_BAND_66 = 262,
+   NR5G_BAND_70 = 263,
+   NR5G_BAND_71 = 264,
+   NR5G_BAND_74 = 265,
+   NR5G_BAND_75 = 266,
+   NR5G_BAND_76 = 267,
+   NR5G_BAND_77 = 268,
+   NR5G_BAND_78 = 269,
+   NR5G_BAND_79 = 270,
+   NR5G_BAND_80 = 271,
+   NR5G_BAND_81 = 272,
+   NR5G_BAND_82 = 273,
+   NR5G_BAND_83 = 274,
+   NR5G_BAND_84 = 275,
+   NR5G_BAND_85 = 276,
+   NR5G_BAND_257 = 277,
+   NR5G_BAND_258 = 278,
+   NR5G_BAND_259 = 279,
+   NR5G_BAND_260 = 280,
+   NR5G_BAND_261 = 281,
+   NR5G_BAND_12 = 282,
+   NR5G_BAND_25 = 283,
+   NR5G_BAND_34 = 284,
+   NR5G_BAND_39 = 285,
+   NR5G_BAND_40 = 286,
+   NR5G_BAND_65 = 287,
+   NR5G_BAND_86 = 288,
+   NR5G_BAND_48 = 289,
+   NR5G_BAND_14 = 290,
+   NR5G_BAND_13 = 291,
+   NR5G_BAND_18 = 292,
+   NR5G_BAND_26 = 293,
+   NR5G_BAND_30 = 294,
+   NR5G_BAND_29 = 295,
+   NR5G_BAND_53 = 296,
+   NR5G_BAND_46 = 297,
+   NR5G_BAND_91 = 298,
+   NR5G_BAND_92 = 299,
+   NR5G_BAND_93 = 300,
+   NR5G_BAND_94 = 301
+};
+
+/**
+ * Defines RF Bandwidth Information.
+ */
+
+enum class RFBandWidth {
+   INVALID_BANDWIDTH = -1,    /**<  Invalid Value */
+   LTE_BW_NRB_6 = 0,          /**<  LTE 1.4 */
+   LTE_BW_NRB_15 = 1,         /**<  LTE 3 */
+   LTE_BW_NRB_25 = 2,         /**<  LTE 5  */
+   LTE_BW_NRB_50 = 3,         /**<  LTE 10 */
+   LTE_BW_NRB_75 = 4,         /**<  LTE 15 */
+   LTE_BW_NRB_100 = 5,        /**<  LTE 20 */
+   NR5G_BW_NRB_5 = 6,         /**<  NR5G 5 */
+   NR5G_BW_NRB_10 = 7,        /**<  NR5G 10 */
+   NR5G_BW_NRB_15 = 8,        /**<  NR5G 15 */
+   NR5G_BW_NRB_20 = 9,        /**<  NR5G 20 */
+   NR5G_BW_NRB_25 = 10,       /**<  NR5G 25 */
+   NR5G_BW_NRB_30 = 11,       /**<  NR5G 30 */
+   NR5G_BW_NRB_40 = 12,       /**<  NR5G 40 */
+   NR5G_BW_NRB_50 = 13,       /**<  NR5G 50 */
+   NR5G_BW_NRB_60 = 14,       /**<  NR5G 60 */
+   NR5G_BW_NRB_80 = 15,       /**<  NR5G 80 */
+   NR5G_BW_NRB_90 = 16,       /**<  NR5G 90 */
+   NR5G_BW_NRB_100 = 17,      /**<  NR5G 100 */
+   NR5G_BW_NRB_200 = 18,      /**<  NR5G 200 */
+   NR5G_BW_NRB_400 = 19,      /**<  NR5G 400 */
+   GSM_BW_NRB_2 = 20,         /**<  GSM  0.2 */
+   TDSCDMA_BW_NRB_2 = 21,     /**<  TDSCDMA 1.6 */
+   WCDMA_BW_NRB_5 = 22,       /**<  WCDMA 5 */
+   WCDMA_BW_NRB_10 = 23,      /**<  WCDMA 10 */
+   NR5G_BW_NRB_70 = 24        /**<  NR5G 70 */
+};
+
+/**
+ * Defines information of RF bands.
+ */
+struct RFBandInfo {
+   RFBand band;            /**< Currently active band */
+   uint32_t channel;       /**< Currently active channel */
+   RFBandWidth bandWidth;  /**< Bandwidth information */
 };
 
 /** @} */ /* end_addtogroup telematics_serving_system */
@@ -387,6 +562,17 @@ public:
     *             could break backwards compatibility.
     */
    virtual void onNetworkTimeChanged(NetworkTimeInfo info) {
+   }
+
+   /**
+    * This function is called whenever the RF band information changes.
+    *
+    * @param [in] bandInfo       @ref telux::tel::RFBandInfo
+    *
+    * @note Eval: This is a new API and is being evaluated. It is subject to change and
+    *             could break backwards compatibility.
+    */
+   virtual void onRFBandInfoChanged(RFBandInfo bandInfo) {
    }
 
    /**

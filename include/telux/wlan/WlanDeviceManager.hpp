4f259bc54f58f9e9a93d6362f1b05dfb7fec2052
Vishal Sadashiv Shetty <vishalsa@qti.qualcomm.com>
2025-12-19 14:44:23 +0530

    wlan: added support to dynamically update wlan mode
    - added new setMode & getCurrentConfig API
    
    CRs-Fixed: 4294481
    
    Change-Id: If26eca17dfd6346e8b061d54c3715f306c5e1360

--- include/telux/wlan/WlanDeviceManager.hpp
+++ include/telux/wlan/WlanDeviceManager.hpp
@@ -1,42 +1,13 @@
 /*
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted (subject to the limitations in the
- * disclaimer below) provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *
- *     * Redistributions in binary form must reproduce the above
- *       copyright notice, this list of conditions and the following
- *       disclaimer in the documentation and/or other materials provided
- *       with the distribution.
- *
- *     * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
- *       contributors may be used to endorse or promote products derived
- *       from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
- * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
- * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
- * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
- * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
 /**
  * @file       WlanDeviceManager.hpp
  *
- * @brief      WlanDeviceManager is a primary interface for configuring WLAN (Wireless Local
- *             Area Network). it provide APIs for configuring WLAN connectivity.
+ * @brief      WlanDeviceManager is a primary interface for configuring Wlan (Wireless Local
+ *             Area Network). it provide APIs for configuring Wlan connectivity.
  *
  */
 
@@ -84,6 +55,26 @@ struct InterfaceStatus {
     std::vector<StaStatus> staStatus;           /**< Vector of active Sta status  */
 };
 
+/**
+ * Configuration for setting Wlan mode with extended parameters.
+ */
+struct ModeConfig {
+    int numOfAp = 0;                    /**< Number of Access Points to be enabled.
+                                            If no Access Point is intended to be enabled, this
+                                            argument should be set to 0. Configuration of each
+                                            AP is accomplished through
+                                            @ref telux::wlan::getApInterfaceManager() instance
+                                            requested from factory. */
+    int numOfSta = 0;                   /**< Number of Stations to be enabled. If no station is
+                                            enabled, this argument should be set to 0.
+                                            Configuration of each Station is accomplished
+                                            through @ref telux::wlan::getStaInterfaceManager()
+                                            instance requested from factory. */
+    bool isPersistent = false;          /**< If true, mode configuration persists across
+                                             Wlan restarts. */
+    bool updateImmediately = false;     /**< If true, changes apply without Wlan restart */
+};
+
 /** @addtogroup telematics_wlan
  * @{ */
 
@@ -94,11 +85,11 @@ struct InterfaceStatus {
 class IWlanDeviceManager {
  public:
     /**
-     * Checks the readiness status of wlan manager and returns the result.
+     * Checks the readiness status of Wlan manager and returns the result.
      *
-     * @returns SERVICE_AVAILABLE    -  If wlan manager is ready for service.
-     *          SERVICE_UNAVAILABLE  -  If wlan manager is temporarily unavailable.
-     *          SERVICE_FAILED       -  If wlan manager encountered an irrecoverable failure.
+     * @returns SERVICE_AVAILABLE    -  If Wlan manager is ready for service.
+     *          SERVICE_UNAVAILABLE  -  If Wlan manager is temporarily unavailable.
+     *          SERVICE_FAILED       -  If Wlan manager encountered an irrecoverable failure.
      *
      * @note Eval: This is a new API and is being evaluated. It is subject to change and
      *             could break backwards compatibility.
@@ -107,16 +98,17 @@ class IWlanDeviceManager {
 
     /**
      * Enable or Disable Wlan Service.
-     * Configurations set by @ref telux::wlan::IWlanDeviceManager::setMode must be completed before
-     * enabling Wlan.
-     * If any of configurations need to be changed after Wlan is enabled, this API must be called
-     * with enable set to false followed by a call with enable set to true for the new
-     * configurations to take effect.
+     * Persistent mode configurations set by @ref telux::wlan::IWlanDeviceManager::setMode must be
+     * completed before enabling Wlan. Dynamic mode configurations can be applied without requiring
+     * Wlan restart.
+     * If any of the persistent mode configurations need to be changed after Wlan is enabled, this
+     * API must be called with enable set to false followed by a call with enable set to true
+     * for the new mode configurations to take effect.
      * Calling this API with enable, will start hostapd and wpa_supplicant daemons.
      * Further changes to hostapd and wpa_supplicant will require calling
      * @ref telux::wlan::IApInterfaceManager::manageApService and
      * @ref telux::wlan::IStaInterfaceManager::manageStaService respectively.
-     * Client shall wait for @ref IWlanListener::onEnableChanged indication to confirm WLAN was
+     * Client shall wait for @ref IWlanListener::onEnableChanged indication to confirm Wlan was
      * enabled/disabled successfully
      *
      * On platforms with Access control enabled, Caller needs to have TELUX_WLAN_DEVICE_CONFIG
@@ -132,18 +124,46 @@ class IWlanDeviceManager {
     virtual  telux::common::ErrorCode enable(bool enable) = 0;
 
     /**
-     * Set Wlan mode - number of supported APs, and stations.
-     * This API shall be called when wlan is disabled. On enablement, wlan will enable APs and
-     * Stations set in this API.
+     * Configure Wlan operating mode with dynamic update capability.
+     * This API handles two distinct mode configuration types with different behaviors:
      *
-     * @param [in] numOfAp           Num of Access Points to be enabled. If no Access Point is
-     *                               enabled, this argument should be set to 0.
-     *                               Configuration of each AP is accomplished through
-     *                               telux::data::wlan::IApManager instance requested from factory.
-     * @param [in] numOfSta          Num of Stations to be enabled. If no station is enabled,
-     *                               this argument should be set to 0.
-     *                               Configuration of each Station is accomplished through
-     *                               telux::data::wlan::IStaManager instance requested from factory.
+     * 1. Persistent mode configuration (@ref telux::wlan::ModeConfig::isPersistent = true):
+     *    - Is persistent across Wlan restarts.
+     *    - Retrieved by @ref telux::wlan::IWlanDeviceManager::getConfig().
+     *    - Applied during Wlan enablement.
+     *
+     * 2. Dynamic mode configuration (@ref telux::wlan::ModeConfig::updateImmediately = true):
+     *    - Active only during current session.
+     *    - Retrieved by @ref telux::wlan::IWlanDeviceManager::getCurrentConfig().
+     *    - Takes effect immediately without Wlan restart.
+     *    - Lost on Wlan restart unless also saved as persistent.
+     *
+     * Behavior depends on Wlan state:
+     *
+     * When Wlan is disabled:
+     *   - Persistent mode configuration can be set
+     *     (@ref telux::wlan::ModeConfig::isPersistent = true).
+     *   - Dynamic changes (@ref telux::wlan::ModeConfig::updateImmediately = true)
+     *     return ERROR_INVALID_STATE.
+     *   - Mode Configuration is applied on next Wlan enablement.
+     *
+     * When Wlan is enabled:
+     *   - If updateImmediately=true:
+     *       * Changes take effect immediately without restart.
+     *       * If @ref telux::wlan::ModeConfig::isPersistent = true, mode configuration is also
+     *         saved so that it takes effect on the next start of Wlan.
+     *       * If @ref telux::wlan::ModeConfig::isPersistent = false, changes only
+     *         apply while the Wlan is active. When Wlan is disabled and enabled again, it will
+     *         revert to persistent config/mode.
+     *   - If @ref telux::wlan::ModeConfig::updateImmediately=false:
+     *       * Requires Wlan restart for the changes to take effect.
+     *       * If @ref telux::wlan::ModeConfig::isPersistent = true, saved mode configuration
+     *         will apply on next enable.
+     *
+     * This API executes synchronously. The update is atomic. Any failure results in a rollback of
+     * the WLAN configuration to its original state.
+     *
+     * @param [in] config     Mode configuration parameters @ref telux::wlan::ModeConfig
      *
      * On platforms with Access control enabled, Caller needs to have TELUX_WLAN_DEVICE_CONFIG
      * permission to invoke this API successfully.
@@ -153,15 +173,16 @@ class IWlanDeviceManager {
      * @note    Eval: This is a new API and is being evaluated. It is subject to change
      *          and could break backwards compatibility.
      */
-    virtual  telux::common::ErrorCode setMode(int numOfAp, int numOfSta) = 0;
+    virtual telux::common::ErrorCode setMode(const ModeConfig &config) = 0;
 
     /**
-     * Request Wlan configuration: Returns the configuration that was set using
-     * @ref telus::wlan::IWlanDeviceManager::setMode.
-     * This might differ from what configuration is has actually been enabled in the system, for
-     * instance, when the hardware cannot fully support the configuration that was set.
-     * To get the status of current configuration an Wlan enablement,
-     * @ref telux::wlan::IWlanDeviceManager::getStatus should be used.
+     * Retrieve the persistent Wlan mode configuration.
+     *
+     * This API returns the mode configuration that persists across Wlan restarts,
+     * as set using @ref telux::wlan::IWlanDeviceManager::setMode with isPersistent=true.
+     *
+     * @note This may differ from the mode configuration currently active in the system. To check
+     * the current Wlan status and enablement, use @ref telux::wlan::IWlanDeviceManager::getStatus.
      *
      * @param [in] numAp                Num of configured APs
      * @param [in] numSta               Num of configured Stations
@@ -172,6 +193,25 @@ class IWlanDeviceManager {
      *           break backwards compatibility.
      */
     virtual telux::common::ErrorCode getConfig(int& numAp, int& numSta) = 0;
+
+    /**
+     * Retrieve the current Wlan mode configuration.
+     *
+     * This API returns the mode configuration that was most recently set using
+     * @ref telux::wlan::IWlanDeviceManager::setMode and is dynamic.
+     *
+     * @note This may differ from the mode configuration currently active in the system. To check
+     * the current Wlan status and enablement, use @ref telux::wlan::IWlanDeviceManager::getStatus.
+     *
+     * @param [in] numAp                Num of configured APs
+     * @param [in] numSta               Num of configured Stations
+     *
+     * @returns operation error code (if any). @ref telux::common::ErrorCode
+     *
+     * @note     Eval: This is a new API and is being evaluated.It is subject to change and could
+     *           break backwards compatibility.
+     */
+    virtual telux::common::ErrorCode getCurrentConfig(int& numAp, int& numSta) = 0;
 
     /**
      * Request Wlan status: Return Wlan enablement status and Interface status of APs and Station
@@ -215,6 +255,29 @@ class IWlanDeviceManager {
      * Destructor for IWlanDeviceManager
      */
     virtual ~IWlanDeviceManager(){};
+
+    /**
+     * Set Wlan mode - number of supported APs, and stations.
+     * This API shall be called when Wlan is disabled. On enablement, Wlan will enable APs and
+     * Stations set in this API. The Wlan mode configured using this API is persistent.
+     *
+     * @param [in] numOfAp           Num of Access Points to be enabled. If no Access Point is
+     *                               enabled, this argument should be set to 0.
+     *                               Configuration of each AP is accomplished through
+     *                               telux::data::wlan::IApManager instance requested from factory.
+     * @param [in] numOfSta          Num of Stations to be enabled. If no station is enabled,
+     *                               this argument should be set to 0.
+     *                               Configuration of each Station is accomplished through
+     *                               telux::data::wlan::IStaManager instance requested from factory.
+     *
+     * On platforms with Access control enabled, Caller needs to have TELUX_WLAN_DEVICE_CONFIG
+     * permission to invoke this API successfully.
+     *
+     * @returns operation error code (if any). @ref telux::common::ErrorCode.
+     *
+     * @deprecated   Use telux::wlan::setMode(const ModeConfig&)
+     */
+    virtual  telux::common::ErrorCode setMode(int numOfAp, int numOfSta) = 0;
 };  // end of IWlanDeviceManager
 
 

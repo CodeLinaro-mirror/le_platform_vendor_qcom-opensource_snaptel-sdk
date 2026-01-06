0fd380f03a9bc8be93a17a2fb8627248dffb617e
Amisha Singh <amissing@qti.qualcomm.com>
2026-01-06 22:36:46 +0530

    tel: Add onDtmfReceived listener
    - Add onDtmfReceived listener to ICallListener
    
    CRs-Fixed: 4408444
    Change-Id: I654c753b465094ce9427df828793b595ca8f3eb7

--- include/telux/tel/CallListener.hpp
+++ include/telux/tel/CallListener.hpp
@@ -28,39 +28,9 @@
  */
 
 /*
- *  Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
- *
- *  Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted (subject to the limitations in the
- * disclaimer below) provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *
- *     * Redistributions in binary form must reproduce the above
- *       copyright notice, this list of conditions and the following
- *       disclaimer in the documentation and/or other materials provided
- *       with the distribution.
- *
- *     * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
- *       contributors may be used to endorse or promote products derived
- *       from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
- * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
- * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
- * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
- * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ​​​​​Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
 /**
@@ -322,6 +292,29 @@ public:
     *          could break backward compatibility.
     */
    virtual void onECallRedial(int phoneId, ECallRedialInfo info) {}
+
+   /**
+     * This function is invoked upon detection of a DTMF tone at the receiving endpoint during PS
+     * calls. For CS call, @ref IVoiceListener::onDtmfToneDetection is invoked as DTMF tone is
+     * delivered as part of signaling information.
+     * @ref IVoiceListener::onDtmfToneDetection is used in conjuction with
+     * @ref IAudioVoiceStream::registerListener().
+     *
+     * On platforms with access control enabled, the caller needs to have TELUX_TEL_CALL_INFO_READ
+     * permission to receive this notification.
+     *
+     * @param [in] phoneId    Represents the phone corresponding to which the DTMF digit
+     *                        was received
+     * @param [in] dtmfTone   Represents single character.
+     *                        Valid values:
+     *                        • Digits: 0–9
+     *                        • Special characters: '\*' and '#'
+     *
+     * @note    Eval: This is a new API and is being evaluated. It is subject to change and
+     *          could break backward compatibility.
+     */
+    virtual void onDtmfReceived(int phoneId, char dtmfTone) {
+    }
 
    virtual ~ICallListener() {
    }

4ef2212ffad39e6469dc6ffbcf6b11b9ee86ee09
Raxit Raju <raxiraju@qti.qualcomm.com>
2026-01-12 10:27:25 +0530

    satcom: Support for NTN enablement in telux_power_refd.
    - Using telux_power_refd, allow TCU to go to suspend power state
    for extended periods of time when NTN is enabled.
    Also, allow TCU to be woken up and put in resume state
    when MT SMS over NTN message is received.
    
    - Implement controlled cleanup for Telux Power Refd.
    
    - Fix warnings treated as errors for null checks and
    possible dangling temporary strings.
    
    - Enabled the satcom feature guard macro for apps under public.
    
    CRs-Fixed: 4337439, 4320301, 4365953, 4382361
    
    Change-Id: I6d1ffb1108fcf47c15d29ff7593c0ef019b7373e

--- apps/reference/power_ref_app/common/socket/ConnectionHandler.cpp
+++ apps/reference/power_ref_app/common/socket/ConnectionHandler.cpp
@@ -24,6 +24,7 @@ ConnectionHandler::~ConnectionHandler() {
     for (const auto &entry : connectionConfigList_) {
         entry->dataConnectionManager = nullptr;
     }
+    connectionConfigList_ = {};
 }
 
 std::shared_ptr<ConnectionHandler> ConnectionHandler::getInstance() {
@@ -130,8 +131,11 @@ void ConnectionHandler::cleanup() {
     isCleanupTriggered_ = true;
     cvStatusUpdate_.notify_all();
     for (const auto &entry : connectionConfigList_) {
-        entry->socketConnection->cleanup();
+        if(entry->socketConnection)
+            entry->socketConnection->cleanup();
+        entry->dataConnectionManager = nullptr;
     }
+    connectionConfigList_ = {};
 }
 
 void ConnectionHandler::onDataCallInfoChanged(

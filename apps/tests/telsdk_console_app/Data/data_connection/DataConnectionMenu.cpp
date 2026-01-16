c929892453e405528268ce1f104c872e2cfbdaa6
Raxit Raju <raxiraju@qti.qualcomm.com>
2026-01-16 17:52:24 +0530

    data: Added support to retrieve throughput information.
    Added APIs and interfaces to configure throughput report interval
    and retrieve uplink and downlink throughput information.
    
    CRs-Fixed: 4366206
    
    Change-Id: Ie6d01c05871837ff5385468ac683119ba23baf4f

--- apps/tests/telsdk_console_app/Data/data_connection/DataConnectionMenu.cpp
+++ apps/tests/telsdk_console_app/Data/data_connection/DataConnectionMenu.cpp
@@ -118,11 +118,25 @@ bool DataConnectionMenu::init() {
     std::shared_ptr<ConsoleAppCommand> requestThrottledAPNInfo
         = std::make_shared<ConsoleAppCommand>(ConsoleAppCommand("14", "request_throttled_apn_info",
             {}, std::bind(&DataConnectionMenu::requestThrottledApnsInfo, this)));
+    std::shared_ptr<ConsoleAppCommand> registerForThroughput
+        = std::make_shared<ConsoleAppCommand>(ConsoleAppCommand("15", "register_for_throughput", {},
+            std::bind(&DataConnectionMenu::registerForThroughput, this, std::placeholders::_1)));
+    std::shared_ptr<ConsoleAppCommand> deregisterForThroughput
+        = std::make_shared<ConsoleAppCommand>(ConsoleAppCommand("16", "deregister_for_throughput",
+            {},
+            std::bind(&DataConnectionMenu::deregisterForThroughput, this, std::placeholders::_1)));
+    std::shared_ptr<ConsoleAppCommand> setThroughputInterval
+        = std::make_shared<ConsoleAppCommand>(ConsoleAppCommand("17", "set_throughput_interval", {},
+            std::bind(&DataConnectionMenu::setThroughputInterval, this, std::placeholders::_1)));
+    std::shared_ptr<ConsoleAppCommand> getLastThroughputInfo = std::make_shared<ConsoleAppCommand>(
+        ConsoleAppCommand("18", "get_last_throughput_info", {},
+            std::bind(&DataConnectionMenu::getLastThroughputInfo, this, std::placeholders::_1)));
 
     std::vector<std::shared_ptr<ConsoleAppCommand>> commandsList = {startDataCall, stopDataCall,
         reqDataCallStats, resetDataCallStats, reqDataCallList, setDefaultProfile, getDefaultProfile,
         reqDataCallBitRate, setRoamingMode, requestRoamingMode, requestTrafficFlowTemplate,
-        startDataCall_V1, stopDataCall_V1, requestThrottledAPNInfo};
+        startDataCall_V1, stopDataCall_V1, requestThrottledAPNInfo, registerForThroughput,
+        deregisterForThroughput, setThroughputInterval, getLastThroughputInfo};
 
     addCommands(commandsList);
     return dcmSubSystemStatus;
@@ -820,4 +834,100 @@ void DataConnectionMenu::requestThrottledApnsInfo() {
     retStat = dataConnectionManagerMap_[static_cast<SlotId>(slotId)]->requestThrottledApnInfo(
         MyDataCallResponseCallback::requestThrottledApnInfoCb);
     Utils::printStatus(retStat);
+}
+
+void DataConnectionMenu::registerForThroughput(std::vector<std::string> inputCommand) {
+    std::cout << "\nRegister For Throughput" << std::endl;
+    telux::common::Status retStat;
+    int slotId = DEFAULT_SLOT_ID;
+    if (telux::common::DeviceConfig::isMultiSimSupported()) {
+        slotId = Utils::getValidSlotId();
+    }
+    if (dataConnectionManagerMap_.find(static_cast<SlotId>(slotId))
+        == dataConnectionManagerMap_.end()) {
+        std::cout << "\nData Connection Manager on slot " << slotId << " is not ready" << std::endl;
+        return;
+    }
+    DataConnectionIndications indicationList;
+    indicationList.set(telux::data::DataConnectionIndicationsType::THROUGHPUT);
+    retStat = dataConnectionManagerMap_[static_cast<SlotId>(slotId)]->registerListener(
+        dataListeners_[static_cast<SlotId>(slotId)], indicationList);
+    Utils::printStatus(retStat);
+}
+
+void DataConnectionMenu::deregisterForThroughput(std::vector<std::string> inputCommand) {
+    std::cout << "\nDeregister for Throughput" << std::endl;
+    telux::common::Status retStat;
+    int slotId = DEFAULT_SLOT_ID;
+    if (telux::common::DeviceConfig::isMultiSimSupported()) {
+        slotId = Utils::getValidSlotId();
+    }
+    if (dataConnectionManagerMap_.find(static_cast<SlotId>(slotId))
+        == dataConnectionManagerMap_.end()) {
+        std::cout << "\nData Connection Manager on slot " << slotId << " is not ready" << std::endl;
+        return;
+    }
+    DataConnectionIndications indicationList;
+    indicationList.set(telux::data::DataConnectionIndicationsType::THROUGHPUT);
+    retStat = dataConnectionManagerMap_[static_cast<SlotId>(slotId)]->deregisterListener(
+        dataListeners_[static_cast<SlotId>(slotId)], indicationList);
+    Utils::printStatus(retStat);
+}
+
+void DataConnectionMenu::setThroughputInterval(std::vector<std::string> inputCommand) {
+    std::cout << "\nSet Throughput Interval" << std::endl;
+    int slotId = DEFAULT_SLOT_ID;
+    if (telux::common::DeviceConfig::isMultiSimSupported()) {
+        slotId = Utils::getValidSlotId();
+    }
+    if (dataConnectionManagerMap_.find(static_cast<SlotId>(slotId))
+        == dataConnectionManagerMap_.end()) {
+        std::cout << "\nData Connection Manager on slot " << slotId << " is not ready" << std::endl;
+        return;
+    }
+    uint32_t reportInterval;
+    std::cout << "Enter reportInterval: ";
+    std::cin >> reportInterval;
+    telux::common::ErrorCode error
+        = dataConnectionManagerMap_[static_cast<SlotId>(slotId)]->setThroughputInterval(
+            reportInterval);
+    std::cout << "ErrorCode: " << static_cast<int>(error)
+              << ", description: " << Utils::getErrorCodeAsString(error) << std::endl;
+}
+
+void DataConnectionMenu::getLastThroughputInfo(std::vector<std::string> inputCommand) {
+    std::cout << "\nGet Last Throughput Info" << std::endl;
+    telux::common::Status retStat;
+    int slotId = DEFAULT_SLOT_ID;
+    if (telux::common::DeviceConfig::isMultiSimSupported()) {
+        slotId = Utils::getValidSlotId();
+    }
+    if (dataConnectionManagerMap_.find(static_cast<SlotId>(slotId))
+        == dataConnectionManagerMap_.end()) {
+        std::cout << "\nData Connection Manager on slot " << slotId << " is not ready" << std::endl;
+        return;
+    }
+    std::vector<telux::data::ThroughputInfo> info = {};
+    telux::common::ErrorCode error
+        = dataConnectionManagerMap_[static_cast<SlotId>(slotId)]->getLastThroughputInfo(info);
+    std::cout << "ErrorCode: " << static_cast<int>(error)
+              << ", description: " << Utils::getErrorCodeAsString(error) << std::endl;
+    if (error == telux::common::ErrorCode::SUCCESS) {
+        std::cout << "---------------------------------------------------------------"
+                  << "\n";
+        std::cout << "Throughput details \n";
+        std::cout << "---------------------------------------------------------------"
+                  << "\n";
+        for (size_t i = 0; i < info.size(); i++) {
+            std::cout << "------------Profile ID: " << info[i].profileId << "-------------"
+                      << "\n";
+            std::cout << "UL throughput: " << info[i].ulThroughput.throughput << "\n";
+            std::cout << "UL max throughput: " << info[i].ulThroughput.maxThroughput << "\n";
+            std::cout << "UL queue size: " << info[i].ulThroughput.queueSize << "\n";
+            std::cout << "DL throughput: " << info[i].dlThroughput.throughput << "\n";
+            std::cout << "Slot ID: " << static_cast<int>(info[i].slot) << "\n";
+        }
+        std::cout << "---------------------------------------------------------------"
+                  << "\n";
+    }
 }

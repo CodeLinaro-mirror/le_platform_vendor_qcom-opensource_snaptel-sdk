1a467c49c256e350b7e7b5ca7aed823a05c79cb1
Maulik Hinsu <mhinsu@qti.qualcomm.com>
2026-01-26 09:15:13 -0800

    audio: Fix for repeated playback crash
     - The FormatParams for StreamConfig in AMR-WB+ is unused in the PAL, so it is being set to null to
       avoid leaving a wild pointer.
    
    CRs-Fixed: 4345790
    Change-Id: I3454cf9e8482409c9becbc6fc991bcbba2ff2dce
    (cherry picked from commit e33535f9026bb565ed2d85732de4f1eb274fef83)

--- include/telux/audio/AudioDefines.hpp
+++ include/telux/audio/AudioDefines.hpp
@@ -413,7 +413,7 @@ struct StreamConfig {
     std::vector<Direction> voicePaths;
 
     /** @ref FormatParams - defines compressed playback format */
-    FormatParams *formatParams;
+    FormatParams *formatParams = nullptr;
 
     /** @ref EcnrMode - true to enable ECNR on an ecall */
     EcnrMode ecnrMode = EcnrMode::DISABLE;

7b04753f28842b25c5d8a016f7084f6138828985
Rahul Pasapula <rpasapul@qti.qualcomm.com>
2026-03-11 15:45:38 +0530

    telux: Support for RTX DGNSS data format
    - Added support for new GNSS data source format DATA_FORMAT_RTX.
    
    CRs-Fixed: 4439514
    
    Change-Id: Iecb18683a20e6ff3fe481520a6d5f9db0c93f597
    (cherry picked from commit d6dfeca7359c43e20c906ed5bba9990f906acdbd)

--- apps/tests/location_test_app/DgnssMenu.hpp
+++ apps/tests/location_test_app/DgnssMenu.hpp
@@ -27,6 +27,12 @@
  *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/*
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
 #ifndef DGNSSMENU_HPP
 #define DGNSSMENU_HPP
 
@@ -72,8 +78,8 @@ private:
    std::shared_ptr<IDgnssManager> dgnssManager_ = nullptr;
    int ntcSocketFd_ = -1;
    int dgnssSourceFd_ = -1;
-   DgnssSourceType dgnssSourceType_;
-
+   DgnssSourceType dgnssSourceType_ = DgnssSourceType::FILE_SOURCE;
+   DgnssDataFormat dataFormat_ = DgnssDataFormat::DATA_FORMAT_UNKNOWN;
 
 };
 #endif  // DGNSMENU_HPP
